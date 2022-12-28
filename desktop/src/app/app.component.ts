12c221dd5d90a83bc8b5fe615e54865237196f46
Noah <noah231515@gmail.com>
2022-12-28 10:25:49 -0500

    set up login page a bit more

--- desktop/src/app/app.component.ts
+++ desktop/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
-import { AuthState } from 'src/store/auth.state';
 import { SetAuthState } from 'src/store/auth.state.actions';
 
 @Component({
@@ -13,7 +12,6 @@ export class AppComponent implements OnInit {
   constructor(private store: Store) {}
   public ngOnInit(): void {
     this.store.dispatch(new SetAuthState());
-    this.store.select((state) => state).subscribe((v) => console.warn(v));
     // get fresh toekn if need to
   }
 }
