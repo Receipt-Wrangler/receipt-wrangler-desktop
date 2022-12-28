3923221f0ae6f7bc5baf2c486b02f2d196a12607
Noah <noah231515@gmail.com>
2022-12-28 14:31:53 -0500

    get routing and guard setup

--- desktop/src/app/app-routing.module.ts
+++ desktop/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from 'src/guards/auth.guard';
+// set up dashboard
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('../auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full',
   },
 ];
 
---
+++ desktop/src/guards/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AuthState } from 'src/store/auth.state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private store: Store) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const isLoggedIn = this.store.selectSnapshot(AuthState.isLoggedIn);
+    const navigatingToAuth = route.url.toString().includes('auth');
+
+    // if user tries to go to login screens while already logged in
+    if (navigatingToAuth && isLoggedIn) {
+      this.router.navigate(['/dashboard']);
+      return false;
+    } else if (navigatingToAuth && !isLoggedIn) {
+      return true;
+    }
+
+    return isLoggedIn;
+  }
+}
