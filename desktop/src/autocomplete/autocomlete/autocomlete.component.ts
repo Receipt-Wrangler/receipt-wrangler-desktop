780da3000f10fcc8d792bcf4f58154f693a52b6d
Noah <noah231515@gmail.com>
2022-12-31 21:19:01 -0500

    stub autocomplete

---
+++ desktop/src/autocomplete/autocomlete/autocomlete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomlete',
+  templateUrl: './autocomlete.component.html',
+  styleUrls: ['./autocomlete.component.scss']
+})
+export class AutocomleteComponent {
+
+}
---
+++ desktop/src/autocomplete/autocomplete.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutocomleteComponent } from './autocomlete/autocomlete.component';
+
+
+
+@NgModule({
+  declarations: [
+    AutocomleteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AutocompleteModule { }
