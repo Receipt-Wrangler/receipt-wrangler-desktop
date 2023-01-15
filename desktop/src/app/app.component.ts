52d28c68257e2ba946d5076b06c047efac11d53a
Noah <noah231515@gmail.com>
2023-01-15 09:22:12 -0500

    clean up how we get refresh tokens

--- desktop/src/app/app.component.ts
+++ desktop/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventType, Router } from '@angular/router';
 import { Store } from '@ngxs/store';
+import { switchMap, take, tap } from 'rxjs';
 import { AuthService } from 'src/api/auth.service';
 import { UsersService } from 'src/api/users.service';
 import { AuthState } from 'src/store/auth.state';
@@ -17,22 +19,50 @@ export class AppComponent implements OnInit {
   constructor(
     private store: Store,
     private authService: AuthService,
-    private userService: UsersService
+    private userService: UsersService,
+    private router: Router
   ) {}
 
   public ngOnInit(): void {
+    this.listenForNavigationStart();
     this.store.dispatch(new SetAuthState());
-    const isTokenExpired = this.store.selectSnapshot(AuthState.isTokenExpired);
-    const isLoggedIn = this.store.selectSnapshot(AuthState.isLoggedIn);
+    const hasToken = this.store.selectSnapshot(AuthState.token);
+    let isLoggedIn = this.store.selectSnapshot(AuthState.isLoggedIn);
 
-    //if (isTokenExpired) {
-    this.authService.getNewRefreshToken().subscribe();
-    //}
-
-    if (isLoggedIn) {
-      this.userService.getAllUsers().subscribe((users) => {
-        this.store.dispatch(new SetUsers(users));
-      });
+    if (hasToken) {
+      this.authService
+        .getNewRefreshToken()
+        .pipe(
+          take(1),
+          tap(() => {
+            isLoggedIn = this.store.selectSnapshot(AuthState.isLoggedIn);
+          }),
+          switchMap(() => {
+            return this.userService.getAllUsers().pipe(take(1));
+          }),
+          switchMap((users) => this.store.dispatch(new SetUsers(users)))
+        )
+        .subscribe();
+    } else {
+      this.userService
+        .getAllUsers()
+        .pipe(take(1))
+        .subscribe((users) => {
+          this.store.dispatch(new SetUsers(users));
+        });
     }
+  }
+
+  private listenForNavigationStart(): void {
+    this.router.events.subscribe((e: any) => {
+      if (e.type === EventType.NavigationStart) {
+        const tokenExpired = this.store.selectSnapshot(
+          AuthState.isTokenExpired
+        );
+        if (tokenExpired) {
+          this.authService.getNewRefreshToken().pipe(take(1)).subscribe();
+        }
+      }
+    });
   }
 }
