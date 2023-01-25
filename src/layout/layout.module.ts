8251b17b0ec891734d0e590cc26ae227f6bb7f01
Noah <noah231515@gmail.com>
2023-01-24 22:05:20 -0500

    add icon buttons

--- src/layout/layout.module.ts
+++ src/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatTooltipModule],
   exports: [HeaderComponent],
 })
 export class LayoutModule {}
