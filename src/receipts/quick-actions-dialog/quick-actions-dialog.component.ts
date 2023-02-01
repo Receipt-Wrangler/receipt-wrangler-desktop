250041682b1ab8bc8227cc443d4db1b7be8a71da
Noah <noah231515@gmail.com>
2023-02-01 11:32:08 -0500

    change language

--- src/receipts/quick-actions-dialog/quick-actions-dialog.component.ts
+++ src/receipts/quick-actions-dialog/quick-actions-dialog.component.ts
@@ -109,7 +109,7 @@ export class QuickActionsDialogComponent implements OnInit {
     users.forEach((u) => {
       const item = this.buildSplitItem(
         u,
-        `${u.displayName}'s even Split`,
+        `${u.displayName}'s Even Portion`,
         Number.parseFloat((receiptAmount / users.length).toFixed(2))
       );
 
@@ -130,11 +130,11 @@ export class QuickActionsDialogComponent implements OnInit {
       const userOptionalPart = Number.parseFloat(
         this.localForm.get(u.id.toString())?.value
       );
-      if (!Number.isNaN(userOptionalPart)) {
+      if (!Number.isNaN(userOptionalPart) || Number(userOptionalPart) === 0) {
         amount -= userOptionalPart;
         const item = this.buildSplitItem(
           u,
-          `${u.displayName}'s Optional Part`,
+          `${u.displayName}'s Portion`,
           this.localForm.get(u.id.toString())?.value
         );
         const formGroup = buildItemForm(
