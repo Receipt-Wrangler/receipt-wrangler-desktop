91151591fe6d780588ee712c7ecb8423d1f7ebe2
Noah <noah231515@gmail.com>
2023-02-06 08:32:28 -0500

    get users on the screen

--- src/user/user-list/user-list.component.ts
+++ src/user/user-list/user-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { User } from 'src/models';
+import { UserState } from 'src/store/user.state';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent {
+  @Select(UserState.users) public users!: Observable<User[]>;
 
+  public displayedColumns = ['username', 'displayname'];
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserRoutingModule } from './user-routing.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, MatTableModule, UserRoutingModule],
 })
 export class UserModule {}
