cf38a5d0abd2ccfe137a8f37128f7dc746c9a247
Noah <noah231515@gmail.com>
2023-02-08 08:19:57 -0500

    cancel button

--- src/shared-ui/shared-ui.module.ts
+++ src/shared-ui/shared-ui.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'src/button/button.module';
 import { SubmitButtonComponent } from './submit-button/submit-button.component';
+import { CancelButtonComponent } from './cancel-button/cancel-button.component';
 
 @NgModule({
   declarations: [
-    SubmitButtonComponent
+    SubmitButtonComponent,
+    CancelButtonComponent
   ],
   imports: [CommonModule, ButtonModule],
   exports: [
-    SubmitButtonComponent
+    SubmitButtonComponent,
+    CancelButtonComponent
   ],
 })
 export class SharedUiModule {}
