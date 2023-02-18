2caa812782d7a8f4df8f4534ef51d88f90cf21ba
Noah <noah231515@gmail.com>
2023-02-18 16:47:55 -0500

    put duration on snackbar

--- constants/snackbar.constant.ts
+++ constants/snackbar.constant.ts
@@ -5,4 +5,5 @@ export const DEFAULT_SNACKBAR_ACTION: string = 'Ok';
 export const DEFAULT_SNACKBAR_CONFIG: MatSnackBarConfig<any> = {
   horizontalPosition: 'right',
   verticalPosition: 'top',
+  duration: 3000,
 };
