49fb3ab710a47a1a187e16499362b1b7b6ad1f77
Noah <noah231515@gmail.com>
2023-03-13 22:40:37 -0400

    make it look nice

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -4,9 +4,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MatCardModule } from '@angular/material/card';
 import { PipesModule } from 'src/pipes/pipes.module';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, MatCardModule, PipesModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatCardModule,
+    PipesModule,
+    MatListModule,
+  ],
 })
 export class DashboardModule {}
