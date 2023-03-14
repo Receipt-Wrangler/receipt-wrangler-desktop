3198934e88cc9f75b063db590e0d14f807a3673b
Noah <noah231515@gmail.com>
2023-03-14 09:08:29 -0400

    group tests

--- src/group/group-form/group-form.component.spec.ts
+++ src/group/group-form/group-form.component.spec.ts
@@ -1,18 +1,26 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxsModule } from '@ngxs/store';
+import { of } from 'rxjs';
 import { ButtonModule } from 'src/button/button.module';
+import { GroupRole } from 'src/enums/group-role.enum';
 import { InputModule } from 'src/input/input.module';
+import { FormGetPipe } from 'src/pipes/form-get.pipe';
 import { PipesModule } from 'src/pipes/pipes.module';
+import { SelectModule } from 'src/select/select.module';
 import { SharedUiModule } from 'src/shared-ui/shared-ui.module';
 import { TableModule } from 'src/table/table.module';
+import { UserAutocompleteModule } from 'src/user-autocomplete/user-autocomplete.module';
+import { GroupMemberFormComponent } from '../group-member-form/group-member-form.component';
+import { buildGroupMemberForm } from '../utils/group-member.utils';
 import { GroupFormComponent } from './group-form.component';
 
 fdescribe('GroupFormComponent', () => {
@@ -21,7 +29,7 @@ fdescribe('GroupFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GroupFormComponent],
+      declarations: [GroupFormComponent, GroupMemberFormComponent],
       imports: [
         ButtonModule,
         HttpClientTestingModule,
@@ -34,8 +42,10 @@ fdescribe('GroupFormComponent', () => {
         PipesModule,
         ReactiveFormsModule,
         RouterTestingModule.withRoutes([]),
+        SelectModule,
         SharedUiModule,
         TableModule,
+        UserAutocompleteModule,
       ],
       providers: [
         {
@@ -54,10 +64,123 @@ fdescribe('GroupFormComponent', () => {
 
     fixture = TestBed.createComponent(GroupFormComponent);
     component = fixture.componentInstance;
+    component.table = {
+      sort: new MatSort(),
+    } as any;
     // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add user to group member on add', () => {
+    const matDialog = TestBed.inject(MatDialog);
+    const formGroup = buildGroupMemberForm();
+    formGroup.patchValue({
+      userId: '2',
+      groupId: '1',
+      groupRole: GroupRole.VIEWER,
+    });
+    spyOn(matDialog, 'open').and.returnValue({
+      afterClosed: () => of(formGroup),
+      componentInstance: { currentGroupMembers: [] },
+    } as any);
+    component.ngOnInit();
+    component.ngAfterViewInit();
+
+    component.addGroupMemberClicked();
+
+    expect(component.groupMembers.value).toEqual([formGroup.value]);
+    expect(component.dataSource.data).toEqual([formGroup.value]);
+  });
+
+  it('should update form on edit', () => {
+    const result = [
+      {
+        userId: 2,
+        groupId: 1,
+        groupRole: GroupRole.OWNER,
+      },
+      {
+        userId: 3,
+        groupId: 1,
+        groupRole: GroupRole.OWNER,
+      },
+    ];
+    const matDialog = TestBed.inject(MatDialog);
+    const formGroup = buildGroupMemberForm();
+    formGroup.patchValue({
+      userId: 3,
+      groupId: 1,
+      groupRole: GroupRole.OWNER,
+    });
+    spyOn(matDialog, 'open').and.returnValue({
+      afterClosed: () => of(formGroup),
+      componentInstance: { currentGroupMembers: [] },
+    } as any);
+    const route = TestBed.inject(ActivatedRoute);
+    route.snapshot.data = {
+      group: {
+        id: 1,
+        name: 'test',
+        isDefault: true,
+        groupMembers: [
+          {
+            userId: 2,
+            groupId: 1,
+            groupRole: GroupRole.OWNER,
+          },
+          {
+            userId: 1,
+            groupId: 1,
+            groupRole: GroupRole.VIEWER,
+          },
+        ],
+      },
+      formConfig: {},
+    };
+
+    component.ngOnInit();
+    component.ngAfterViewInit();
+
+    component.editGroupMemberClicked(1);
+
+    expect(component.groupMembers.value).toEqual(result);
+    expect(component.dataSource.data).toEqual(result as any);
+  });
+
+  it('should remove user to group member on remove', () => {
+    const route = TestBed.inject(ActivatedRoute);
+    const result = {
+      userId: 1,
+      groupId: 1,
+      groupRole: GroupRole.VIEWER,
+    };
+
+    route.snapshot.data = {
+      group: {
+        id: 1,
+        name: 'test',
+        isDefault: true,
+        groupMembers: [
+          {
+            userId: 2,
+            groupId: 1,
+            groupRole: GroupRole.OWNER,
+          },
+          result,
+        ],
+      },
+      formConfig: {},
+    };
+
+    component.ngOnInit();
+    component.ngAfterViewInit();
+
+    component.removeGroupMember(0);
+
+    expect(component.groupMembers.value).toEqual([result]);
+    expect(component.dataSource.data).toEqual([result] as any);
   });
 });
