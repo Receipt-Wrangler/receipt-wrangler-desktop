fb24a8485d86a81b2b55b2c5ae8bee9fd9817a57
Noah <noah231515@gmail.com>
2023-03-14 08:35:22 -0400

    get test setup

--- src/group/group-form/group-form.component.spec.ts
+++ src/group/group-form/group-form.component.spec.ts
@@ -1,19 +1,60 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxsModule } from '@ngxs/store';
+import { ButtonModule } from 'src/button/button.module';
+import { InputModule } from 'src/input/input.module';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { SharedUiModule } from 'src/shared-ui/shared-ui.module';
+import { TableModule } from 'src/table/table.module';
 import { GroupFormComponent } from './group-form.component';
 
-describe('GroupFormComponent', () => {
+fdescribe('GroupFormComponent', () => {
   let component: GroupFormComponent;
   let fixture: ComponentFixture<GroupFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GroupFormComponent],
+      imports: [
+        ButtonModule,
+        HttpClientTestingModule,
+        InputModule,
+        MatCardModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        NgxsModule.forRoot([]),
+        NoopAnimationsModule,
+        PipesModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]),
+        SharedUiModule,
+        TableModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                group: {},
+                formConfig: {},
+              },
+            },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GroupFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
