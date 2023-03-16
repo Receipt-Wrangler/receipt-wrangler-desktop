5344cd03f8105cf520553d278e700a1adf595ce0
Noah <noah231515@gmail.com>
2023-03-16 09:13:11 -0400

    pass in id

--- src/receipts/receipts.module.ts
+++ src/receipts/receipts.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { AutocompleteModule } from 'src/autocomplete/autocomplete.module';
+import { AvatarModule } from 'src/avatar';
 import { ButtonModule } from 'src/button/button.module';
 import { CarouselModule } from 'src/carousel/carousel.module';
 import { DatepickerModule } from 'src/datepicker/datepicker.module';
@@ -55,7 +57,9 @@ import { UserTotalPipe } from './user-total.pipe';
     SlideToggleModule,
     UserAutocompleteModule,
     TableModule,
+    MatCardModule,
     TextareaModule,
+    AvatarModule,
   ],
 })
 export class ReceiptsModule {}
