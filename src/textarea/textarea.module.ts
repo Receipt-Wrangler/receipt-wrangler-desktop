d8046694eecd4d47aedad13b3992fd6b2fb526f1
Noah <noah231515@gmail.com>
2023-03-16 09:12:43 -0400

    add placeholder

--- src/textarea/textarea.module.ts
+++ src/textarea/textarea.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { TextareaComponent } from './textarea/textarea.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [TextareaComponent],
-  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   exports: [TextareaComponent],
 })
 export class TextareaModule {}
