4e34e09e5ca6fc6f1658b1c2dd07ae35a2f95bbb
Noah <noah231515@gmail.com>
2023-03-16 08:35:32 -0400

    set up form

--- src/receipts/receipt-comments/receipt-comments.component.ts
+++ src/receipts/receipt-comments/receipt-comments.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormMode } from 'src/enums/form-mode.enum';
 import { Comment } from '../../models/comment';
 
 @Component({
@@ -6,6 +8,27 @@ import { Comment } from '../../models/comment';
   templateUrl: './receipt-comments.component.html',
   styleUrls: ['./receipt-comments.component.scss'],
 })
-export class ReceiptCommentsComponent {
+export class ReceiptCommentsComponent implements OnInit {
   @Input() public comments: Comment[] = [];
+  @Input() public mode!: FormMode;
+
+  public commentsArray: FormArray<any> = new FormArray<any>([]);
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.comments.forEach((c) => {
+      this.commentsArray.push(this.buildCommentFormGroup(c));
+    });
+  }
+
+  private buildCommentFormGroup(comment?: Comment): FormGroup {
+    return this.formBuilder.group({
+      comment: [comment?.comment ?? ''],
+    });
+  }
 }
--- src/receipts/receipts.module.ts
+++ src/receipts/receipts.module.ts
@@ -1,32 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReceiptsTableComponent } from './receipts-table/receipts-table.component';
-import { ReceiptsRoutingModule } from './receipts-routing.module';
-import { MatTableModule } from '@angular/material/table';
-import { PipesModule } from 'src/pipes/pipes.module';
-import { SlideToggleModule } from 'src/slide-toggle/slide-toggle.module';
-import { ButtonModule } from 'src/button/button.module';
-import { ReceiptFormComponent } from './receipt-form/receipt-form.component';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { InputModule } from 'src/input/input.module';
-import { AutocompleteModule } from 'src/autocomplete/autocomplete.module';
-import { DatepickerModule } from 'src/datepicker/datepicker.module';
-import { ItemListComponent } from './item-list/item-list.component';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { UserTotalPipe } from './user-total.pipe';
-import { QuickActionsDialogComponent } from './quick-actions-dialog/quick-actions-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { RadioGroupModule } from 'src/radio-group/radio-group.module';
-import { UserAutocompleteModule } from 'src/user-autocomplete/user-autocomplete.module';
-import { UploadImageComponent } from './upload-image/upload-image.component';
-import { CarouselModule } from 'src/carousel/carousel.module';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { AutocompleteModule } from 'src/autocomplete/autocomplete.module';
+import { ButtonModule } from 'src/button/button.module';
+import { CarouselModule } from 'src/carousel/carousel.module';
+import { DatepickerModule } from 'src/datepicker/datepicker.module';
+import { InputModule } from 'src/input/input.module';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { RadioGroupModule } from 'src/radio-group/radio-group.module';
 import { SharedUiModule } from 'src/shared-ui/shared-ui.module';
+import { SlideToggleModule } from 'src/slide-toggle/slide-toggle.module';
 import { TableModule } from 'src/table/table.module';
+import { TextareaModule } from 'src/textarea/textarea.module';
+import { UserAutocompleteModule } from 'src/user-autocomplete/user-autocomplete.module';
+import { ItemListComponent } from './item-list/item-list.component';
+import { QuickActionsDialogComponent } from './quick-actions-dialog/quick-actions-dialog.component';
 import { ReceiptCommentsComponent } from './receipt-comments/receipt-comments.component';
+import { ReceiptFormComponent } from './receipt-form/receipt-form.component';
+import { ReceiptsRoutingModule } from './receipts-routing.module';
+import { ReceiptsTableComponent } from './receipts-table/receipts-table.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+import { UserTotalPipe } from './user-total.pipe';
 
 @NgModule({
   declarations: [
@@ -57,6 +55,7 @@ import { ReceiptCommentsComponent } from './receipt-comments/receipt-comments.co
     SlideToggleModule,
     UserAutocompleteModule,
     TableModule,
+    TextareaModule,
   ],
 })
 export class ReceiptsModule {}
