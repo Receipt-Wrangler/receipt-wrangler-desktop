009c2f02f4d0e415ea68abee41281efeb32c0064
Noah <noah231515@gmail.com>
2023-03-15 21:02:09 -0400

    pass in comments

--- src/receipts/receipt-comments/receipt-comments.component.ts
+++ src/receipts/receipt-comments/receipt-comments.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Comment } from '../../models/comment';
 
 @Component({
   selector: 'app-receipt-comments',
   templateUrl: './receipt-comments.component.html',
-  styleUrls: ['./receipt-comments.component.scss']
+  styleUrls: ['./receipt-comments.component.scss'],
 })
 export class ReceiptCommentsComponent {
-
+  @Input() public comments: Comment[] = [];
 }
