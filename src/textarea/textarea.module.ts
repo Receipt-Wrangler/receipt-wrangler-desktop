94b6e592b6b6aa97467acd050a17be7cac347cc2
Noah <noah231515@gmail.com>
2023-03-16 08:20:34 -0400

    build out initial text area

--- src/textarea/textarea.module.ts
+++ src/textarea/textarea.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextareaComponent } from './textarea/textarea.component';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    TextareaComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    TextareaComponent
-  ]
+  declarations: [TextareaComponent],
+  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule],
+  exports: [TextareaComponent],
 })
-export class TextareaModule { }
+export class TextareaModule {}
--- src/textarea/textarea/textarea.component.ts
+++ src/textarea/textarea/textarea.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BaseInputComponent } from 'src/base-input/base-input/base-input.component';
+import { InputInterface } from 'src/input/input.interface';
 
 @Component({
   selector: 'app-textarea',
   templateUrl: './textarea.component.html',
-  styleUrls: ['./textarea.component.scss']
+  styleUrls: ['./textarea.component.scss'],
 })
-export class TextareaComponent {
-
-}
+export class TextareaComponent
+  extends BaseInputComponent
+  implements InputInterface {}
