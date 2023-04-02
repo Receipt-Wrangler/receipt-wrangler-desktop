79af2a65d3fac3128023c2d806f2cadb8aeb2e3f
Noah <noah231515@gmail.com>
2023-04-02 11:03:37 -0400

    get basic layout

--- src/layout/layout.module.ts
+++ src/layout/layout.module.ts
@@ -1,33 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterModule } from '@angular/router';
-import { AvatarModule } from '../avatar';
-import { HeaderComponent } from './header/header.component';
-import { AddReceiptIconComponent } from './add-receipt-icon/add-receipt-icon.component';
-import { ReceiptListIconComponent } from './receipt-list-icon/receipt-list-icon.component';
-import { DashboardIconComponent } from './dashboard-icon/dashboard-icon.component';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { DirectivesModule } from 'src/directives/directives.module';
-import { SwitchGroupDialogComponent } from './switch-group-dialog/switch-group-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 import { AutocompleteModule } from 'src/autocomplete/autocomplete.module';
-import { SharedUiModule } from 'src/shared-ui/shared-ui.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { DirectivesModule } from 'src/directives/directives.module';
 import { PipesModule } from 'src/pipes/pipes.module';
+import { SharedUiModule } from 'src/shared-ui/shared-ui.module';
+import { AvatarModule } from '../avatar';
+import { AddReceiptIconComponent } from './add-receipt-icon/add-receipt-icon.component';
+import { DashboardIconComponent } from './dashboard-icon/dashboard-icon.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceiptListIconComponent } from './receipt-list-icon/receipt-list-icon.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SwitchGroupDialogComponent } from './switch-group-dialog/switch-group-dialog.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     AddReceiptIconComponent,
-    ReceiptListIconComponent,
     DashboardIconComponent,
-    SwitchGroupDialogComponent,
+    HeaderComponent,
+    ReceiptListIconComponent,
     SidebarComponent,
+    SwitchGroupDialogComponent,
   ],
   imports: [
     AutocompleteModule,
@@ -44,6 +45,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     ReactiveFormsModule,
     RouterModule,
     SharedUiModule,
+    MatSidenavModule,
   ],
   exports: [HeaderComponent, AddReceiptIconComponent],
 })
--- src/layout/sidebar/sidebar.component.ts
+++ src/layout/sidebar/sidebar.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
