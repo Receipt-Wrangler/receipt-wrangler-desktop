da46ce58d9c7a10a1b56011e54ca11663635bc75
Noah <noah231515@gmail.com>
2023-04-12 20:01:02 -0400

    more

--- src/guards/feature.guard.spec.ts
+++ src/guards/feature.guard.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { NgxsModule } from '@ngxs/store';
 import { FeatureGuard } from './feature.guard';
 
 describe('FeatureGuard', () => {
   let guard: FeatureGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([])],
+    });
     guard = TestBed.inject(FeatureGuard);
   });
 
--- src/guards/group-role.guard.spec.ts
+++ src/guards/group-role.guard.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GroupRoleGuard } from './group-role.guard';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxsModule } from '@ngxs/store';
 
 describe('GroupRoleGuard', () => {
   let guard: GroupRoleGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, NgxsModule.forRoot([])],
+    });
     guard = TestBed.inject(GroupRoleGuard);
   });
 
