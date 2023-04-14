42226c25cb1c9972e3e8fc6ba6462bced284e331
Noah <noah231515@gmail.com>
2023-04-14 08:39:25 -0400

    browser

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', "ChromeHeadlessCI"],
     singleRun: false,
     restartOnFileChange: true
   });
