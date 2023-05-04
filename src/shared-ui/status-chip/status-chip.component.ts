ebba5f13e0f66791c464f04444eca66531ce0978
Noah <noah231515@gmail.com>
2023-05-04 08:39:07 -0400

    add status chip to table

--- src/shared-ui/status-chip/status-chip.component.ts
+++ src/shared-ui/status-chip/status-chip.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ReceiptStatus } from 'src/enums/receipt-status.enum';
 
 @Component({
   selector: 'app-status-chip',
@@ -7,4 +8,6 @@ import { Component, Input } from '@angular/core';
 })
 export class StatusChipComponent {
   @Input() public status: string = "";
+
+  public receiptStatus = ReceiptStatus;
 }
