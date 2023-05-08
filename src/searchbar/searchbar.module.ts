7746e55149a30c62492c6c4a9f2546a4ebba0d03
Noah <noah231515@gmail.com>
2023-05-08 08:19:41 -0400

    stub out search

--- src/searchbar/searchbar.module.ts
+++ src/searchbar/searchbar.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchbarComponent } from './searchbar/searchbar.component';
-
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    SearchbarComponent
-  ],
+  declarations: [SearchbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
-  exports: [
-    SearchbarComponent
-  ]
+  exports: [SearchbarComponent],
 })
-export class SearchbarModule { }
+export class SearchbarModule {}
--- src/searchbar/searchbar/searchbar.component.ts
+++ src/searchbar/searchbar/searchbar.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { tap } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
 })
 export class SearchbarComponent {
+  public results: any[] = [];
 
+  public searchFormControl = new FormControl('');
+
+  public displayFn = () => {
+    return '';
+  };
+
+  public ngOnInit(): void {
+    this.searchFormControl.valueChanges
+      .pipe(
+        untilDestroyed(this),
+        tap((value) => {
+          console.warn(value);
+        })
+      )
+      .subscribe();
+  }
 }
