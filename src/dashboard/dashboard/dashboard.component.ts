1cb8f73ac95a35e9c7a59d0f00721c9f495ff1df
Noah <noah231515@gmail.com>
2023-05-16 08:51:13 -0400

    get receipts for group

--- src/dashboard/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { ReceiptsService } from 'src/api/receipts.service';
+import { GroupState } from 'src/store/group.state';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  constructor(private receiptsService: ReceiptsService, private store: Store) {}
+
+  public ngOnInit(): void {
+    this.getReceipts();
+  }
+
+  private getReceipts(): void {
+    const currentGroupId = this.store.selectSnapshot(
+      GroupState.selectedGroupId
+    );
+    this.receiptsService.getReceiptsForGroupIds([currentGroupId]).subscribe();
+  }
+}
