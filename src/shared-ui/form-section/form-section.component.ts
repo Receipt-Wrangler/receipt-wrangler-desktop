f9eead89f9de5e5387df441328c9fbad0e3663e9
Noah <noah231515@gmail.com>
2023-05-18 08:33:31 -0400

    add button template to form section

--- src/shared-ui/form-section/form-section.component.ts
+++ src/shared-ui/form-section/form-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-form-section',
@@ -7,4 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class FormSectionComponent {
   @Input() public headerText: string = '';
+
+  @Input() public headerButtonsTemplate?: TemplateRef<any>;
 }
