a4ca5c2920b183c657dae11e3b4d7c154f3d015b
Noah <noah231515@gmail.com>
2023-05-23 08:48:27 -0400

    get basics set up

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -5,9 +5,21 @@ import { SettingsRoutingModule } from './settings-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { InputModule } from 'src/input/input.module';
+import { SharedUiModule } from 'src/shared-ui/shared-ui.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PipesModule } from 'src/pipes/pipes.module';
 
 @NgModule({
   declarations: [SettingsComponent, UserProfileComponent],
-  imports: [CommonModule, SettingsRoutingModule, MatTabsModule],
+  imports: [
+    CommonModule,
+    SettingsRoutingModule,
+    MatTabsModule,
+    InputModule,
+    SharedUiModule,
+    ReactiveFormsModule,
+    PipesModule,
+  ],
 })
 export class SettingsModule {}
--- src/settings/user-profile/user-profile.component.ts
+++ src/settings/user-profile/user-profile.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Select, Store } from '@ngxs/store';
+import { User } from 'src/models';
+import { AuthState } from 'src/store/auth.state';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  public form: FormGroup = new FormGroup({});
 
+  public user!: User;
+
+  constructor(private formBuilder: FormBuilder, private store: Store) {}
+
+  public ngOnInit(): void {
+    this.user = this.store.selectSnapshot(AuthState.loggedInUser);
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.formBuilder.group({
+      username: this.user?.username ?? '',
+      displayName: this.user?.displayName ?? '',
+    });
+  }
 }
