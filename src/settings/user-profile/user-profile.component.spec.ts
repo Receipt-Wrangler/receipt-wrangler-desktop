28fbf9f101dd3174ce0b658c9ca598654a6af3dd
Noah <noah231515@gmail.com>
2023-05-25 08:12:55 -0400

    add tests

--- src/settings/user-profile/user-profile.component.spec.ts
+++ src/settings/user-profile/user-profile.component.spec.ts
@@ -3,11 +3,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserProfileComponent } from './user-profile.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { PipesModule } from 'src/pipes/pipes.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthState } from 'src/store/auth.state';
+import { UsersService } from 'src/api/users.service';
+import { of } from 'rxjs';
+import { AuthService } from 'src/api/auth.service';
+import { UserState } from 'src/store/user.state';
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
@@ -19,7 +24,7 @@ describe('UserProfileComponent', () => {
       imports: [
         HttpClientModule,
         MatSnackBarModule,
-        NgxsModule.forRoot([]),
+        NgxsModule.forRoot([AuthState, UserState]),
         PipesModule,
         ReactiveFormsModule,
       ],
@@ -34,10 +39,84 @@ describe('UserProfileComponent', () => {
 
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should init form correctly', () => {
+    const store = TestBed.inject(Store);
+    store.reset({
+      auth: {
+        username: 'cheetos',
+        displayname: 'burger',
+      },
+    });
+
+    component.ngOnInit();
+
+    expect(component.form.value).toEqual({
+      username: 'cheetos',
+      displayName: 'burger',
+    });
+  });
+
+  it('should submit form and update state correctly', () => {
+    const store = TestBed.inject(Store);
+    const serviceSpy = spyOn(TestBed.inject(UsersService), 'updateUserProfile');
+    const authSpy = spyOn(TestBed.inject(AuthService), 'getNewRefreshToken');
+
+    spyOn(
+      TestBed.inject(UsersService),
+      'getClaimsForLoggedInUser'
+    ).and.returnValue(
+      of({
+        userId: '1',
+        displayname: 'store',
+        username: 'general',
+      })
+    );
+
+    serviceSpy.and.returnValue(of(undefined));
+    authSpy.and.returnValue(of(undefined));
+
+    store.reset({
+      users: { users: [{ id: 1, displayName: 'cheetos', username: 'burger' }] },
+      auth: {
+        userId: '1',
+        username: 'cheetos',
+        displayname: 'burger',
+      },
+    });
+
+    component.ngOnInit();
+    component.form.patchValue({
+      username: 'general',
+      displayName: 'store',
+    });
+
+    component.submit();
+
+    const updatedUser = store.selectSnapshot(AuthState.loggedInUser);
+    const updatedUsers = store.selectSnapshot(UserState.users);
+
+    expect(serviceSpy).toHaveBeenCalledWith({
+      username: 'general',
+      displayName: 'store',
+    } as any);
+    expect(authSpy).toHaveBeenCalled();
+
+    // expect(updatedUser).toEqual({
+    //   id: 1,
+    //   username: 'general',
+    //   displayName: 'store',
+    // } as any);
+
+    // expect(updatedUsers[0]).toEqual({
+    //   id: 1,
+    //   username: 'general',
+    //   displayName: 'store',
+    // } as any);
   });
 });
