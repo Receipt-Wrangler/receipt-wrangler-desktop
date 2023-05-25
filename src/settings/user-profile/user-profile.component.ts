d59fd7cccb8e1f97125dc5f2c8811002c95381d5
Noah <noah231515@gmail.com>
2023-05-24 20:36:35 -0400

    submit

--- src/settings/user-profile/user-profile.component.ts
+++ src/settings/user-profile/user-profile.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngxs/store';
+import { switchMap, take, tap } from 'rxjs';
+import { AuthService } from 'src/api/auth.service';
+import { UsersService } from 'src/api/users.service';
 import { FormMode } from 'src/enums/form-mode.enum';
 import { FormConfig } from 'src/interfaces';
 import { User } from 'src/models';
+import { SnackbarService } from 'src/services/snackbar.service';
 import { AuthState } from 'src/store/auth.state';
 
 @Component({
@@ -25,9 +29,12 @@ export class UserProfileComponent implements OnInit {
     'Only system admin may change your username.';
 
   constructor(
+    private authService: AuthService,
     private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private snackbarService: SnackbarService,
     private store: Store,
-    private route: ActivatedRoute
+    private usersService: UsersService
   ) {}
 
   public ngOnInit(): void {
@@ -39,11 +46,26 @@ export class UserProfileComponent implements OnInit {
   private initForm(): void {
     this.form = this.formBuilder.group({
       username: this.user?.username ?? '',
-      displayName: this.user?.displayName ?? '',
+      displayName: [this.user?.displayName ?? '', Validators.required],
     });
 
     if (this.formConfig.mode === FormMode.edit) {
       this.form.get('username')?.disable();
+    }
+  }
+
+  public submit(): void {
+    if (this.form.valid) {
+      this.usersService
+        .updateUserProfile(this.form.value)
+        .pipe(
+          take(1),
+          switchMap(() => this.authService.getNewRefreshToken()),
+          tap(() => {
+            this.snackbarService.success('User profile successfully updated');
+          })
+        )
+        .subscribe();
     }
   }
 }
