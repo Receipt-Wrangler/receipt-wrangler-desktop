768fb3cc3dbce10a24b3d9bab8bebe1367ca2f42
Noah <noah231515@gmail.com>
2023-05-24 20:50:54 -0400

    update tests

--- src/settings/settings/settings.component.spec.ts
+++ src/settings/settings/settings.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -8,9 +13,21 @@ describe('SettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
-    })
-    .compileComponents();
+      declarations: [SettingsComponent],
+      imports: [
+        MatTabsModule,
+        MatTooltipModule,
+        ReactiveFormsModule,
+        RouterModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
--- src/settings/user-profile/user-profile.component.spec.ts
+++ src/settings/user-profile/user-profile.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserProfileComponent } from './user-profile.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
@@ -8,9 +15,22 @@ describe('UserProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserProfileComponent ]
-    })
-    .compileComponents();
+      declarations: [UserProfileComponent],
+      imports: [
+        HttpClientModule,
+        MatSnackBarModule,
+        NgxsModule.forRoot([]),
+        PipesModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { data: { formConfig: {} } } },
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
