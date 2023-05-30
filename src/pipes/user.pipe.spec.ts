73d98569926faffe067c5afebe37fd6f67916606
Noah2315 <Noah231515>
2023-05-29 22:26:50 -0500

    user pipe

--- src/pipes/user.pipe.spec.ts
+++ src/pipes/user.pipe.spec.ts
@@ -1,8 +1,44 @@
+import { TestBed } from '@angular/core/testing';
 import { UserPipe } from './user.pipe';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgxsModule, Store } from '@ngxs/store';
+import { UserState } from 'src/store/user.state';
 
 describe('UserPipe', () => {
+  let pipe: UserPipe;
+  let store: Store;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserPipe],
+      imports: [NgxsModule.forRoot([UserState])],
+      providers: [UserPipe],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    store = TestBed.inject(Store);
+    pipe = TestBed.inject(UserPipe);
+
+    store.reset({
+      users: {
+        users: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      },
+    });
+  });
+
   it('create an instance', () => {
-    //const pipe = new UserPipe();
-    //expect(pipe).toBeTruthy();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should return a user', () => {
+    const result = pipe.transform('1');
+
+    expect(result).toEqual({ id: 1 } as any);
+  });
+
+  it('should return a user', () => {
+    const result = pipe.transform('not a user id');
+
+    expect(result).toEqual(undefined);
   });
 });
