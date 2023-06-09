fcf2c18bebddabc4a0715a1d5c0affe442090e1d
Noah <noah231515@gmail.com>
2023-06-09 08:35:16 -0400

    add status

--- src/models/item.ts
+++ src/models/item.ts
@@ -1,7 +1,10 @@
+import { ItemStatus } from 'src/enums/receipt-item.status.enum';
+
 export interface Item {
   name: string;
   chargedToUserId: number;
   receiptId: number;
   amount: number;
   isTaxed: boolean;
+  status: ItemStatus;
 }
