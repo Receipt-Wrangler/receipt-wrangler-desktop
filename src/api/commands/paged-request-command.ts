1917251171a9738291cae47d3aeeacbd37be0634
Noah <noah231515@gmail.com>
2023-06-19 08:19:13 -0400

    get filter set up

--- src/api/commands/paged-request-command.ts
+++ src/api/commands/paged-request-command.ts
@@ -3,4 +3,22 @@ export interface PagedRequestCommand {
   pageSize: number;
   orderBy: string;
   sortDirection: string;
+  filter?: PagedRequestFilter;
+}
+
+interface PagedRequestFilter {
+  name: PagedRequestFilterField;
+  paidBy: PagedRequestFilterField;
+}
+
+interface PagedRequestFilterField {
+  operation: PagedRequestFilterOperation;
+  value: string;
+}
+
+enum PagedRequestFilterOperation {
+  CONTAINS = 'CONTAINS',
+  EQUALS = 'EQUALS',
+  GREATER_THAN = 'GREATER_THAN',
+  LESS_THAN = 'LESS_THAN',
 }
