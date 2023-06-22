270cd2e55d89dfa4ed27aa8dbc61f1ce71819178
Noah <noah231515@gmail.com>
2023-06-22 07:27:35 -0400

    display options based on value

---
+++ src/autocomplete/autocomlete/option-display.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'optionDisplay',
+})
+export class OptionDisplayPipe implements PipeTransform {
+  public transform(
+    option: any,
+    options: any[],
+    optionDisplayKey?: string,
+    optionValueKey?: string
+  ): any {
+    if (optionValueKey && optionDisplayKey) {
+      return options.find((o) => o[optionValueKey] === option)[
+        optionDisplayKey
+      ];
+    }
+
+    return option;
+  }
+}
