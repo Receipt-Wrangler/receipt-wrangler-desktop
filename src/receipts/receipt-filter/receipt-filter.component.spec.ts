acf06e20e88d210b19731e4b6965917215d5cff2
Noah <noah231515@gmail.com>
2023-06-25 10:44:46 -0400

    fix unit tests

--- src/receipts/receipt-filter/receipt-filter.component.spec.ts
+++ src/receipts/receipt-filter/receipt-filter.component.spec.ts
@@ -11,7 +11,10 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxsModule, Store } from '@ngxs/store';
 import { InputModule } from 'src/input/input.module';
 import { PipesModule } from 'src/pipes/pipes.module';
-import { ReceiptTableState } from 'src/store/receipt-table.state';
+import {
+  ReceiptTableState,
+  defaultReceiptFilter,
+} from 'src/store/receipt-table.state';
 import { OperationsPipe } from './operations.pipe';
 import { ReceiptFilterComponent } from './receipt-filter.component';
 import { PagedRequestFilterOperation } from 'src/api/commands/paged-request-command';
@@ -23,40 +26,6 @@ describe('ReceiptFilterComponent', () => {
   let component: ReceiptFilterComponent;
   let fixture: ComponentFixture<ReceiptFilterComponent>;
   let store: Store;
-  const defaultFilter = {
-    date: {
-      operation: PagedRequestFilterOperation.EQUALS,
-      value: '',
-    },
-    name: {
-      operation: PagedRequestFilterOperation.EQUALS,
-      value: '',
-    },
-    amount: {
-      operation: PagedRequestFilterOperation.EQUALS,
-      value: '',
-    },
-    paidBy: {
-      operation: PagedRequestFilterOperation.CONTAINS,
-      value: [],
-    },
-    categories: {
-      operation: PagedRequestFilterOperation.CONTAINS,
-      value: [],
-    },
-    tags: {
-      operation: PagedRequestFilterOperation.CONTAINS,
-      value: [],
-    },
-    status: {
-      operation: PagedRequestFilterOperation.CONTAINS,
-      value: [],
-    },
-    resolvedDate: {
-      operation: PagedRequestFilterOperation.EQUALS,
-      value: '',
-    },
-  };
 
   const filledFilter = {
     date: {
@@ -135,7 +104,7 @@ describe('ReceiptFilterComponent', () => {
   it('should init form with no default initial data', () => {
     component.ngOnInit();
 
-    expect(component.form.value).toEqual(defaultFilter);
+    expect(component.form.value).toEqual(defaultReceiptFilter);
   });
 
   it('should init form with initial data', () => {
@@ -160,7 +129,7 @@ describe('ReceiptFilterComponent', () => {
     expect(component.form.value).toEqual(filledFilter);
 
     component.resetFilter();
-    expect(component.form.value).toEqual(defaultFilter);
+    expect(component.form.value).toEqual(defaultReceiptFilter);
   });
 
   it('should set form in state and close dialog', () => {
