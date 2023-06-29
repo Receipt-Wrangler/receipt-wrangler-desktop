a5b60ea6f9814d0bf8e1921ae440c1c1bdcd7fb9
Noah <noah231515@gmail.com>
2023-06-29 08:33:50 -0400

    adjust header

--- src/settings/settings/settings.component.ts
+++ src/settings/settings/settings.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DEFAULT_HOST_CLASS } from 'src/constants';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
+  host: DEFAULT_HOST_CLASS,
 })
 export class SettingsComponent {
   links = ['User Profile'];
