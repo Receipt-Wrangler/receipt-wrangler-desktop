dbb49c40fb88db708d58cf5e6898bd21f084481c
Noah <noah231515@gmail.com>
2023-07-15 16:38:48 -0400

    only import in app module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthForm } from './sign-up/auth-form.component';
-import { AuthRoutingModule } from './auth-routing.module';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PipesModule } from 'src/pipes/pipes.module';
-import { InputModule } from 'src/input/input.module';
 import { ButtonModule } from 'src/button/button.module';
 import { DirectivesModule } from 'src/directives/directives.module';
-import { ApiModule } from 'src/api-new';
+import { InputModule } from 'src/input/input.module';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthForm } from './sign-up/auth-form.component';
 
 @NgModule({
   declarations: [AuthForm],
   imports: [
-    ApiModule,
     AuthRoutingModule,
     ButtonModule,
     CommonModule,
