1e1f7b26a1b63364e86f432cfa1d3d19ac8bf799
Noah <noah231515@gmail.com>
2023-08-03 17:17:17 -0400

    add token to env

--- setup-ci.sh
+++ setup-ci.sh
@@ -1,3 +1,3 @@
 # Write .npmrc
 echo "@receipt-wrangler:registry=https://npm.pkg.github.com/" > .npmrc
-echo "//npm.pkg.github.com/:_authToken=${{ secrets.GH_PACKAGE_READ_TOKEN_DESKTOP }}" > .npmrc
+echo "//npm.pkg.github.com/:_authToken=$GH_PACKAGE_READ_TOKEN_DESKTOP" > .npmrc
