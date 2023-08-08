56a2dfa57848c513ca1a2cb896070593e128474b
Noah <noah231515@gmail.com>
2023-08-08 09:03:26 -0400

    adding image, styling

--- src/receipts/quick-scan-dialog/quick-scan-dialog.component.ts
+++ src/receipts/quick-scan-dialog/quick-scan-dialog.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FileData } from '@receipt-wrangler/receipt-wrangler-core';
+import { UploadImageComponent } from '../upload-image/upload-image.component';
 
 @Component({
   selector: 'app-quick-scan-dialog',
   templateUrl: './quick-scan-dialog.component.html',
-  styleUrls: ['./quick-scan-dialog.component.scss']
+  styleUrls: ['./quick-scan-dialog.component.scss'],
 })
-export class QuickScanDialogComponent {
+export class QuickScanDialogComponent implements OnInit {
+  @ViewChild(UploadImageComponent)
+  public uploadImageComponent!: UploadImageComponent;
 
+  public form: FormGroup = new FormGroup({});
+
+  public images: FileData[] = [];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.formBuilder.group({
+      paidByUserId: [null, Validators.required],
+    });
+  }
+
+  public fileLoaded(fileData: FileData): void {
+    this.images.push(fileData);
+  }
+
+  public openImageUploadComponent(): void {
+    this.uploadImageComponent.clickInput();
+  }
+
+  public removeImage(index: number): void {
+    this.images.splice(index, 1);
+  }
 }
