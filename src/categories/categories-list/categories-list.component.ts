b64754f6119d0d2178f0e039e2437fb77582fae9
Noah <noah231515@gmail.com>
2023-08-17 10:38:26 -0400

    get data on screen

--- src/categories/categories-list/categories-list.component.ts
+++ src/categories/categories-list/categories-list.component.ts
@@ -1,28 +1,63 @@
+import { take, tap } from "rxjs";
+import { CategoryTableState } from "src/store/category-table.state";
+import { TableColumn } from "src/table/table-column.interface";
+import { TableComponent } from "src/table/table/table.component";
+
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { Store } from "@ngxs/store";
 import {
-  AfterViewInit,
-  Component,
-  TemplateRef,
-  ViewChild,
-} from '@angular/core';
-import { Store } from '@ngxs/store';
-import { TableColumn } from 'src/table/table-column.interface';
-import { TableComponent } from 'src/table/table/table.component';
+  Category, CategoryService, PagedRequestCommand
+} from "@receipt-wrangler/receipt-wrangler-core";
 
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss'],
 })
-export class CategoriesListComponent implements AfterViewInit {
-  @ViewChild('usernameCell') public nameCell!: TemplateRef<any>;
+export class CategoriesListComponent implements OnInit, AfterViewInit {
+  @ViewChild('nameCell') public nameCell!: TemplateRef<any>;
 
   @ViewChild(TableComponent) public table!: TableComponent;
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private categoryService: CategoryService) {}
+
+  public dataSource: MatTableDataSource<Category> =
+    new MatTableDataSource<Category>([]);
 
   public displayedColumns: string[] = [];
 
   public columns: TableColumn[] = [];
+
+  public totalCount: number = 0;
+
+  public headerText: string = 'Categories';
+
+  public ngOnInit(): void {
+    this.initTableData();
+  }
+
+  private initTableData(): void {
+    this.getCategories();
+  }
+
+  private getCategories(): void {
+    const command: PagedRequestCommand = this.store.selectSnapshot(
+      CategoryTableState.state
+    );
+
+    this.categoryService
+      .getPagedCategories(command)
+      .pipe(
+        take(1),
+        tap((pagedData) => {
+          console.warn(pagedData);
+          this.dataSource = new MatTableDataSource<Category>(pagedData.data);
+          this.totalCount = pagedData.totalCount;
+        })
+      )
+      .subscribe();
+  }
 
   public ngAfterViewInit(): void {
     this.initTable();
--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,3 +1,6 @@
+import { SharedUiModule } from "src/shared-ui/shared-ui.module";
+import { TableModule } from "src/table/table.module";
+
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
@@ -6,6 +9,6 @@ import { CategoriesRoutingModule } from "./categories-routing.module";
 
 @NgModule({
   declarations: [CategoriesListComponent],
-  imports: [CommonModule, CategoriesRoutingModule],
+  imports: [TableModule, SharedUiModule, CommonModule, CategoriesRoutingModule],
 })
 export class CategoriesModule {}
