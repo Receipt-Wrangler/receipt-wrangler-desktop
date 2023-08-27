d98d8f5cf8054ce8a2355e8e4c080aad0575a511
Noah <noah231515@gmail.com>
2023-08-27 11:37:26 -0400

    add support fordisabling on load

--- src/shared-ui/submit-button/submit-button.component.ts
+++ src/shared-ui/submit-button/submit-button.component.ts
@@ -1,6 +1,15 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormMode } from 'src/enums/form-mode.enum';
 import { FormButtonComponent } from '../form-button/form-button.component';
+import { Select, Store } from '@ngxs/store';
+import { Observable, Subscribable, Subscription, startWith, tap } from 'rxjs';
+import { LayoutState } from 'src/store/layout.state';
 
 @Component({
   selector: 'app-submit-button',
@@ -8,8 +17,44 @@ import { FormButtonComponent } from '../form-button/form-button.component';
   styleUrls: ['./submit-button.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class SubmitButtonComponent extends FormButtonComponent {
+export class SubmitButtonComponent
+  extends FormButtonComponent
+  implements OnChanges
+{
   @Input() public onlyIcon: boolean = true;
 
+  @Input() public disableOnLoading: boolean = false;
+
   public formMode = FormMode;
+
+  public disabledSubscription!: Subscription;
+
+  public originalDisabledState: boolean = false;
+
+  constructor(private store: Store) {
+    super();
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['disableOnLoading'].currentValue) {
+      this.disabledSubscription = this.store
+        .select(LayoutState.showProgressBar)
+        .pipe(
+          startWith(this.store.selectSnapshot(LayoutState.showProgressBar)),
+          tap((isShown) => {
+            if (isShown) {
+              this.originalDisabledState = this.disabled;
+              this.disabled = true;
+            } else {
+              this.disabled = this.originalDisabledState;
+            }
+          })
+        )
+        .subscribe();
+    } else {
+      if (this.disabledSubscription) {
+        this.disabledSubscription.unsubscribe();
+      }
+    }
+  }
 }
