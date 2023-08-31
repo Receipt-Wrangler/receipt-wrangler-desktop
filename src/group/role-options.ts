91ce1e16b59851b272d65ce278d3e5b058a133bc
Noah <noah231515@gmail.com>
2023-08-31 08:07:37 -0400

    clean up user roles

--- src/group/role-options.ts
+++ src/group/role-options.ts
@@ -1,4 +1,4 @@
-import { GroupMember } from '@receipt-wrangler/receipt-wrangler-core';
+import { GroupMember, User } from '@receipt-wrangler/receipt-wrangler-core';
 import { FormOption } from 'src/interfaces/form-option.interface';
 import { formatStatus } from 'src/utils';
 
@@ -6,6 +6,16 @@ export const GROUP_ROLE_OPTIONS: FormOption[] = Object.keys(
   GroupMember.GroupRoleEnum
 ).map((key) => {
   const value = (GroupMember.GroupRoleEnum as any)[key];
+  return {
+    value: value,
+    displayValue: formatStatus(value),
+  };
+});
+
+export const USER_ROLE_OPTIONS: FormOption[] = Object.keys(
+  User.UserRoleEnum
+).map((key) => {
+  const value = (User.UserRoleEnum as any)[key];
   return {
     value: value,
     displayValue: formatStatus(value),
