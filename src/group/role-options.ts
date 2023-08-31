7d3c76b44029ce2bc3872b7507d67c41993d29df
Noah <noah231515@gmail.com>
2023-08-31 08:03:06 -0400

    format group statuses

--- src/group/role-options.ts
+++ src/group/role-options.ts
@@ -1,3 +1,13 @@
 import { GroupMember } from '@receipt-wrangler/receipt-wrangler-core';
+import { FormOption } from 'src/interfaces/form-option.interface';
+import { formatStatus } from 'src/utils';
 
-export const ROLE_OPTIONS = Object.keys(GroupMember.GroupRoleEnum);
+export const GROUP_ROLE_OPTIONS: FormOption[] = Object.keys(
+  GroupMember.GroupRoleEnum
+).map((key) => {
+  const value = (GroupMember.GroupRoleEnum as any)[key];
+  return {
+    value: value,
+    displayValue: formatStatus(value),
+  };
+});
