68217c88d9664812f3cf2d45370a21efe94709db
Noah <noah231515@gmail.com>
2023-09-13 07:55:54 -0400

    get tests passing

--- src/group/group-settings-email/group-settings-email.component.spec.ts
+++ src/group/group-settings-email/group-settings-email.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GroupSettingsEmailComponent } from './group-settings-email.component';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { PipesModule as CorePipesModule } from '@receipt-wrangler/receipt-wrangler-core';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 describe('GroupSettingsEmailComponent', () => {
   let component: GroupSettingsEmailComponent;
@@ -8,10 +14,30 @@ describe('GroupSettingsEmailComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GroupSettingsEmailComponent]
+      declarations: [GroupSettingsEmailComponent],
+      imports: [MatSnackBarModule, PipesModule, CorePipesModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                formConfig: {},
+              },
+            },
+          },
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
     fixture = TestBed.createComponent(GroupSettingsEmailComponent);
+
     component = fixture.componentInstance;
+    component.form = new FormGroup({
+      emailToRead: new FormControl(''),
+      emailWhiteList: new FormArray([]),
+      subjectLineRegexes: new FormArray([]),
+    });
     fixture.detectChanges();
   });
 
--- src/shared-ui/form-list/form-list.component.spec.ts
+++ src/shared-ui/form-list/form-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormListComponent } from './form-list.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 describe('FormListComponent', () => {
   let component: FormListComponent;
@@ -8,7 +10,9 @@ describe('FormListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FormListComponent]
+      declarations: [FormListComponent],
+      imports: [MatListModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
     fixture = TestBed.createComponent(FormListComponent);
     component = fixture.componentInstance;
