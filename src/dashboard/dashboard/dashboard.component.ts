5802d57abedf7d3f6be504901384d09d47ddd8c8
Noah <noah231515@gmail.com>
2023-11-28 08:07:28 -0500

    get dasbhaords

--- src/dashboard/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Select } from '@ngxs/store';
-import { Dashboard, GroupState } from '@receipt-wrangler/receipt-wrangler-core';
-import { Observable } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import {
+  Dashboard,
+  DashboardService,
+  GroupState,
+} from '@receipt-wrangler/receipt-wrangler-core';
+import { Observable, take, tap } from 'rxjs';
 import { DEFAULT_DIALOG_CONFIG, DEFAULT_HOST_CLASS } from 'src/constants';
 import { DashboardFormComponent } from '../dashboard-form/dashboard-form.component';
 
@@ -12,11 +16,35 @@ import { DashboardFormComponent } from '../dashboard-form/dashboard-form.compone
   styleUrls: ['./dashboard.component.scss'],
   host: DEFAULT_HOST_CLASS,
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   @Select(GroupState.selectedGroupId)
   public selectedGroupId!: Observable<string>;
 
-  constructor(private matDialog: MatDialog) {}
+  public dashboards: Dashboard[] = [];
+
+  constructor(
+    private matDialog: MatDialog,
+    private dashboardService: DashboardService,
+    private store: Store
+  ) {}
+
+  public ngOnInit(): void {
+    this.getDashboards();
+  }
+
+  private getDashboards(): void {
+    const selectedGroupId = this.store.selectSnapshot(
+      GroupState.selectedGroupId
+    );
+
+    this.dashboardService
+      .getDashboardsForUserByGroupId(selectedGroupId)
+      .pipe(
+        take(1),
+        tap(() => {})
+      )
+      .subscribe();
+  }
 
   public openDashboardDialog(dashboard?: Dashboard): void {
     const dialogRef = this.matDialog.open(
