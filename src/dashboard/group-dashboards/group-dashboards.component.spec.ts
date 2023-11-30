b8d733d27a25f5f8b6a5301183d49f10161033e8
Noah <noah231515@gmail.com>
2023-11-30 08:31:17 -0500

    group dashboard tests

--- src/dashboard/group-dashboards/group-dashboards.component.spec.ts
+++ src/dashboard/group-dashboards/group-dashboards.component.spec.ts
@@ -1,15 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  ComponentFixtureNoNgZone,
+  TestBed,
+} from '@angular/core/testing';
 
 import { GroupDashboardsComponent } from './group-dashboards.component';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgxsModule, Store } from '@ngxs/store';
+import { MatDialogModule } from '@angular/material/dialog';
+import {
+  ButtonModule,
+  PipesModule as CorePipesModule,
+  Dashboard,
+  GroupState,
+  SetSelectedDashboardId,
+} from '@receipt-wrangler/receipt-wrangler-core';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BehaviorSubject, of } from 'rxjs';
 
 describe('GroupDashboardsComponent', () => {
   let component: GroupDashboardsComponent;
   let fixture: ComponentFixture<GroupDashboardsComponent>;
+  let store: Store;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GroupDashboardsComponent]
+      declarations: [GroupDashboardsComponent],
+      imports: [
+        CorePipesModule,
+        MatDialogModule,
+        NgxsModule.forRoot([GroupState]),
+        PipesModule,
+        ButtonModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: new BehaviorSubject<Params>({}),
+            snapshot: {
+              data: {
+                dashboards: [],
+              },
+            },
+          },
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
+
+    store = TestBed.inject(Store);
     fixture = TestBed.createComponent(GroupDashboardsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +58,97 @@ describe('GroupDashboardsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set dashboards with dashboards', () => {
+    const dashboards: Dashboard[] = [
+      {
+        id: 1,
+        name: 'test',
+        groupId: 1,
+        userId: 1,
+      },
+    ];
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    activatedRoute.snapshot.data = {
+      dashboards: dashboards,
+    };
+    component.ngOnInit();
+
+    expect(component.dashboards).toEqual(dashboards);
+  });
+
+  it('should set dashboards with dashboards on param change', () => {
+    const dashboards: Dashboard[] = [
+      {
+        id: 1,
+        name: 'test',
+        groupId: 1,
+        userId: 1,
+      },
+    ];
+    const newDashboards: Dashboard[] = [
+      {
+        id: 2,
+        name: 'test',
+        groupId: 1,
+        userId: 1,
+      },
+    ];
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    activatedRoute.snapshot.data = {
+      dashboards: dashboards,
+    };
+    component.ngOnInit();
+
+    expect(component.dashboards).toEqual(dashboards);
+
+    activatedRoute.snapshot.data = {
+      dashboards: newDashboards,
+    };
+
+    (activatedRoute.params as any).next({
+      dashboardId: 2,
+    });
+
+    expect(component.dashboards).toEqual(newDashboards);
+  });
+
+  it('should navigate to selected dashboard', () => {
+    const routerSpy = spyOn(TestBed.inject(Router), 'navigateByUrl');
+    store.reset({
+      groups: {
+        selectedDashboardId: '1',
+      },
+    });
+
+    component.ngOnInit();
+
+    expect(routerSpy).toHaveBeenCalledWith('/dashboard/group/1/1', {
+      skipLocationChange: false,
+      onSameUrlNavigation: 'reload',
+    });
+  });
+
+  it('should not navigate to selected dashboard', () => {
+    const routerSpy = spyOn(TestBed.inject(Router), 'navigateByUrl');
+    store.reset({
+      groups: {
+        selectedDashboardId: undefined,
+      },
+    });
+
+    component.ngOnInit();
+
+    expect(routerSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it('should set selected dashboard id', () => {
+    const store = TestBed.inject(Store);
+    const storeSpy = spyOn(store, 'dispatch');
+
+    component.setSelectedDashboardId(1);
+
+    expect(storeSpy).toHaveBeenCalledWith(new SetSelectedDashboardId('1'));
   });
 });
