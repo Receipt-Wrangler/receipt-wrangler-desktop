7ecd47209c15ee3c28207ddbf1ac2e59b8b9d2e0
Noah <noah231515@gmail.com>
2023-12-18 10:36:33 -0500

    stub

--- src/dashboard/filtered-receipts/filtered-receipts.component.ts
+++ src/dashboard/filtered-receipts/filtered-receipts.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Widget } from '@receipt-wrangler/receipt-wrangler-core';
 
 @Component({
   selector: 'app-filtered-receipts',
   templateUrl: './filtered-receipts.component.html',
-  styleUrls: ['./filtered-receipts.component.scss']
+  styleUrls: ['./filtered-receipts.component.scss'],
 })
 export class FilteredReceiptsComponent {
-
+  @Input() public widget!: Widget;
 }
