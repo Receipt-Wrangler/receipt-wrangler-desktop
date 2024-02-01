40ca7842cb6b3ae35c4de75452ea8b6a79c31461
Noah <noah231515@gmail.com>
2024-02-01 07:23:12 -0500

    bump version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from "ngx-mask";
 
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { APP_INITIALIZER, NgModule } from "@angular/core";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
-  ApiModule,
-  AppInitService,
-  AuthFormUtil,
-  PipesModule,
-  initAppData,
-} from '@receipt-wrangler/receipt-wrangler-core';
-import { IconModule } from '../icon/icon.module';
-import { HttpInterceptorService } from '../interceptors/http-interceptor.service';
-import { LayoutModule } from '../layout/layout.module';
-import { StoreModule } from '../store/store.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+  ApiModule, AppInitService, AuthFormUtil, initAppData, PipesModule
+} from "@receipt-wrangler/receipt-wrangler-core";
+
+import { IconModule } from "../icon/icon.module";
+import { HttpInterceptorService } from "../interceptors/http-interceptor.service";
+import { LayoutModule } from "../layout/layout.module";
+import { StoreModule } from "../store/store.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
