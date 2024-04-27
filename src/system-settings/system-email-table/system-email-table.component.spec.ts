b3789fdc8f15d41c5fe8add8291df14c0685b3c1
Noah <navi0001@protonmail.com>
2024-04-27 08:42:08 -0400

    fix tests

--- src/system-settings/system-email-table/system-email-table.component.spec.ts
+++ src/system-settings/system-email-table/system-email-table.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { ActivatedRoute } from "@angular/router";
+import { NgxsModule } from "@ngxs/store";
+import { SharedUiModule } from "../../shared-ui/shared-ui.module";
+import { SystemEmailTableState } from "../../store/system-email-table.state";
+import { TableModule } from "../../table/table.module";
 
 import { SystemEmailTableComponent } from "./system-email-table.component";
 
@@ -8,13 +15,21 @@ describe("SystemEmailsComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SystemEmailTableComponent]
+      declarations: [SystemEmailTableComponent],
+      imports: [SharedUiModule,
+        HttpClientTestingModule,
+        NgxsModule.forRoot([SystemEmailTableState]),
+        TableModule,
+        NoopAnimationsModule],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {}
+      }]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(SystemEmailTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it("should create", () => {
--- src/system-settings/system-settings/system-settings.component.spec.ts
+++ src/system-settings/system-settings/system-settings.component.spec.ts
@@ -1,23 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from "@angular/router";
+import { SharedUiModule } from "../../shared-ui/shared-ui.module";
 
-import { SystemSettingsComponent } from './system-settings.component';
+import { SystemSettingsComponent } from "./system-settings.component";
 
-describe('SystemSettingsComponent', () => {
+describe("SystemSettingsComponent", () => {
   let component: SystemSettingsComponent;
   let fixture: ComponentFixture<SystemSettingsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SystemSettingsComponent]
+      declarations: [SystemSettingsComponent],
+      imports: [SharedUiModule],
+      providers: [
+
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(SystemSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
