f7db92e2b4f5a133a779769325b6bc4f07cc3a48
Noah <navi0001@protonmail.com>
2024-04-28 15:03:15 -0400

    unit tests

--- src/shared-ui/audit-detail-section/audit-detail-section.component.spec.ts
+++ src/shared-ui/audit-detail-section/audit-detail-section.component.spec.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { SharedUiModule } from "../shared-ui.module";
 
-import { AuditDetailSectionComponent } from './audit-detail-section.component';
+import { AuditDetailSectionComponent } from "./audit-detail-section.component";
 
-describe('AuditDetailSectionComponent', () => {
+describe("AuditDetailSectionComponent", () => {
   let component: AuditDetailSectionComponent;
   let fixture: ComponentFixture<AuditDetailSectionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AuditDetailSectionComponent]
+      declarations: [AuditDetailSectionComponent],
+      imports: [SharedUiModule]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(AuditDetailSectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/system-settings/system-email-form/system-email-form.component.spec.ts
+++ src/system-settings/system-email-form/system-email-form.component.spec.ts
@@ -1,23 +1,82 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { NgxsModule } from "@ngxs/store";
+import { ApiModule, SystemEmail } from "../../open-api";
+import { PipesModule } from "../../pipes";
+import { SharedUiModule } from "../../shared-ui/shared-ui.module";
 
-import { SystemEmailFormComponent } from './system-email-form.component';
+import { SystemEmailFormComponent } from "./system-email-form.component";
 
-describe('SystemEmailFormComponent', () => {
+describe("SystemEmailFormComponent", () => {
   let component: SystemEmailFormComponent;
   let fixture: ComponentFixture<SystemEmailFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SystemEmailFormComponent]
+      declarations: [SystemEmailFormComponent],
+      imports: [
+        ApiModule,
+        HttpClientTestingModule,
+        NgxsModule.forRoot([]),
+        PipesModule,
+        ReactiveFormsModule,
+        SharedUiModule,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                formConfig: {}
+              }
+            }
+          }
+        }
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(SystemEmailFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("init form when there is no original data", () => {
+    component.ngOnInit();
+
+    expect(component.form.value).toEqual({
+      host: null,
+      port: null,
+      username: null,
+      password: null
+    });
+  });
+
+  it("init form when there is original data", () => {
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    activatedRoute.snapshot.data["systemEmail"] = {
+      host: "host",
+      port: 123,
+      username: "username",
+      password: "password"
+    } as SystemEmail;
+    component.ngOnInit();
+
+
+    expect(component.form.value).toEqual({
+      host: "host",
+      port: 123,
+      username: "username",
+      password: "password"
+    });
   });
 });
