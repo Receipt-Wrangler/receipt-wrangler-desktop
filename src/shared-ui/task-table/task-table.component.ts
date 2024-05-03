a1f3c1f183d6ef41958c528bb2d73b905f29923e
Noah <navi0001@protonmail.com>
2024-05-02 20:34:38 -0400

    set up table stub

--- src/shared-ui/task-table/task-table.component.ts
+++ src/shared-ui/task-table/task-table.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { SystemTask } from "../../open-api";
+import { TableColumn } from "../../table/table-column.interface";
 
 @Component({
-  selector: 'app-task-table',
-  templateUrl: './task-table.component.html',
-  styleUrl: './task-table.component.scss'
+  selector: "app-task-table",
+  templateUrl: "./task-table.component.html",
+  styleUrl: "./task-table.component.scss"
 })
-export class TaskTableComponent {
+export class TaskTableComponent implements OnInit, AfterViewInit {
+  @ViewChild("typeCell") public typeCell!: TemplateRef<any>;
 
+  public displayedColumns: string[] = [];
+
+  public columns: TableColumn[] = [];
+
+  public dataSource: MatTableDataSource<SystemTask> = new MatTableDataSource<SystemTask>([]);
+
+  public totalCount: number = 0;
+
+  public ngOnInit(): void {
+    this.getTableData();
+  }
+
+  private getTableData(): void {}
+
+  public ngAfterViewInit(): void {
+    this.initTable();
+  }
+
+  private initTable(): void {
+    this.setColumns();
+  }
+
+  private setColumns(): void {
+    this.columns = [
+      {
+        columnHeader: "Type",
+        matColumnDef: "type",
+        template: this.typeCell,
+        sortable: true
+      }];
+
+    this.displayedColumns = ["type"];
+  }
 }
