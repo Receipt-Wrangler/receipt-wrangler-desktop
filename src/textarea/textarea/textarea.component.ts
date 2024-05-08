8f7798e89164f8ee6eecf44506c01cd1a590fd1a
Noah <navi0001@protonmail.com>
2024-05-08 11:22:36 -0400

    wip implementation of autocomplete trigger from character

--- src/textarea/textarea/textarea.component.ts
+++ src/textarea/textarea/textarea.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from "@angular/core";
 import { MatAutocompleteTrigger } from "@angular/material/autocomplete";
 import { pairwise, tap } from "rxjs";
 import { BaseInputComponent } from "../../base-input";
@@ -20,7 +20,7 @@ export class TextareaComponent
   implements InputInterface, AfterViewInit {
   @ViewChild(MatAutocompleteTrigger) public matAutocompleteTrigger!: MatAutocompleteTrigger;
 
-  @ViewChild("textarea") public textarea!: HTMLTextAreaElement;
+  @ViewChild("nativeTextarea") public textarea!: ElementRef<HTMLTextAreaElement>;
 
   @Input() public options: string[] = [];
 
@@ -43,16 +43,16 @@ export class TextareaComponent
           const difference = this.getStringDifference(prev, current);
           console.warn(difference, "dif");
 
-          if (true) {
+          if (this.shouldShowAutocomplete(prev, current, difference)) {
             this.matAutocompleteTrigger.openPanel();
-            this.filteredOptions = this.options;
+            this.filterOptions();
 
           } else if (current.endsWith(" ")) {
             this.matAutocompleteTrigger.closePanel();
           }
 
           if (this.matAutocompleteTrigger.panelOpen) {
-            // this.filteredOptions = this.options.filter(option => option.toLowerCase().includes(value.toLowerCase()));
+            this.filterOptions();
           }
         }))
       .subscribe();
@@ -60,57 +60,89 @@ export class TextareaComponent
     // selection aspect to close, but also surroundings
   }
 
+  private filterOptions(): void {
+    const index = this.textarea.nativeElement.selectionStart - 1;
+    //TODO:  capture the current word by getting the current selection, and backtracking to find trigger, then filter off of that
+    const currentWord = this.getTriggerWordFromIndex(index);
+    console.warn(currentWord, "current word");
+    this.filteredOptions = this.options.filter(option => option.toLowerCase().includes(currentWord.toLowerCase()));
+    console.warn(this.filteredOptions, "filtered options");
+
+  }
+
+  private getTriggerWordFromIndex(index: number): string {
+    let currentIndex = index;
+    let foundTrigger = false;
+    let result: string[] = [];
+
+    while (currentIndex > 0) {
+      const char = this.inputFormControl.value[currentIndex];
+      if (char === this.trigger) {
+        foundTrigger = true;
+        break;
+      }
+
+      result.unshift(char);
+      currentIndex--;
+    }
+
+    if (foundTrigger) {
+      return result.join("");
+    } else {
+      return "";
+    }
+  }
+
   public getOptionValue(option: string): string {
-    const insertionIndex = this.textarea.selectionEnd;
-    console.warn(this.inputFormControl.value[insertionIndex]);
+    const insertionIndex = this.textarea.nativeElement.selectionEnd;
 
     return this.inputFormControl.value + option;
   }
 
-  public getStringDifference(string1: string, string2: string): number[] {
-    const result: number[] = [];
-    const string1Map = this.getCharacterMap(string1);
-    const string2Map = this.getCharacterMap(string2);
+  public shouldShowAutocomplete(prev: string, current: string, differences: number[]): boolean {
+    for (let i = 0; i < differences.length; i++) {
 
-    string1Map.forEach((value, key) => {
-      const string2Value = string2Map.get(key);
-      if (string2Value) {
-        const difference = value.filter(index => !string2Value.includes(index));
-        result.push(...difference);
-      } else {
-        result.push(...value);
+      if (this.isValidTriggerCharacter(current, differences[i]) || this.getTriggerWordFromIndex(differences[i])) {
+        return true;
       }
-    });
+    }
 
-    string2Map.forEach((value, key) => {
-      const string1Value = string1Map.get(key);
-      if (string1Value) {
-        const difference = value.filter(index => !string1Value.includes(index));
-        result.push(...difference);
-      } else {
-        result.push(...value);
-      }
-    });
-
-
-    return result;
+    return current.endsWith(this.trigger);
   }
 
-  public getCharacterMap(string: string): Map<string, number[]> {
-    const map = new Map<string, number[]>();
-    const characters = string.split("");
+  private isValidTriggerCharacter(string: string, index: number): boolean {
+    const validEndCharacters = [" ", undefined];
+    const frontCharacter = string[index - 1];
+    const backCharacter = string[index + 1];
 
-    characters.forEach((character, index) => {
-      const exists = map.has(character);
-      let value: number[] = [index];
-      if (exists) {
-        value = map.get(character)!;
-        value.push(index);
+    console.warn(string[index], "string[index]");
+    console.warn(frontCharacter, backCharacter, "front and back");
+
+    return string[index] === this.trigger
+      && validEndCharacters.includes(frontCharacter)
+      && validEndCharacters.includes(backCharacter);
+  }
+
+  public getStringDifference(string1: string, string2: string): number[] {
+    const result: number[] = [];
+    let longString;
+    let shortString;
+
+    if (string1.length > string2.length) {
+      longString = string1;
+      shortString = string2;
+    } else {
+      longString = string2;
+      shortString = string1;
+    }
+
+
+    for (let i = 0; i < longString.length; i++) {
+      if (longString[i] !== shortString[i]) {
+        result.push(i);
       }
+    }
 
-      map.set(character, value);
-    });
-
-    return map;
+    return result;
   }
 }
