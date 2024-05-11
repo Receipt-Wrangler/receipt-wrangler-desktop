aea38bf5fd6511e1c78e9f543862aedf16302d47
Noah <navi0001@protonmail.com>
2024-05-11 11:41:54 -0400

    fix unit tests

--- src/receipt-processing-settings/receipt-processing-settings-table/receipt-processing-settings-table.component.spec.ts
+++ src/receipt-processing-settings/receipt-processing-settings-table/receipt-processing-settings-table.component.spec.ts
@@ -1,23 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NgxsModule } from "@ngxs/store";
+import { TableModule } from "../../table/table.module";
 
-import { ReceiptProcessingSettingsTableComponent } from './receipt-processing-settings-table.component';
+import { ReceiptProcessingSettingsTableComponent } from "./receipt-processing-settings-table.component";
 
-describe('ReceiptProcessingSettingsTableComponent', () => {
+describe("ReceiptProcessingSettingsTableComponent", () => {
   let component: ReceiptProcessingSettingsTableComponent;
   let fixture: ComponentFixture<ReceiptProcessingSettingsTableComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReceiptProcessingSettingsTableComponent]
+      declarations: [ReceiptProcessingSettingsTableComponent],
+      imports: [NgxsModule.forRoot(), HttpClientTestingModule, TableModule],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ReceiptProcessingSettingsTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/receipt-processing-settings/receipt-processing-settings-table/receipt-processing-settings-table.service.spec.ts
+++ src/receipt-processing-settings/receipt-processing-settings-table/receipt-processing-settings-table.service.spec.ts
@@ -1,16 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { NgxsModule } from "@ngxs/store";
+import { TableModule } from "../../table/table.module";
 
-import { ReceiptProcessingSettingsTableService } from './receipt-processing-settings-table.service';
+import { ReceiptProcessingSettingsTableService } from "./receipt-processing-settings-table.service";
 
-describe('ReceiptProcessingSettingsTableService', () => {
+describe("ReceiptProcessingSettingsTableService", () => {
   let service: ReceiptProcessingSettingsTableService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, NgxsModule.forRoot(), TableModule],
+    });
     service = TestBed.inject(ReceiptProcessingSettingsTableService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
