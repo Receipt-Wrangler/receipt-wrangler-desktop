ab8d45ded888d20e4a58389b24bfc363df3b71a9
Noah <navi0001@protonmail.com>
2024-05-19 10:58:03 -0400

    view

--- src/system-settings/system-settings-form/system-settings-form.component.ts
+++ src/system-settings/system-settings-form/system-settings-form.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { tap } from "rxjs";
+import { AutocomleteComponent } from "../../autocomplete/autocomlete/autocomlete.component";
 import { BaseFormComponent } from "../../form";
 import { ReceiptProcessingSettings, SystemSettings } from "../../open-api";
 
+
+@UntilDestroy()
 @Component({
   selector: "app-system-settings-form",
   templateUrl: "./system-settings-form.component.html",
   styleUrl: "./system-settings-form.component.scss"
 })
 export class SystemSettingsFormComponent extends BaseFormComponent implements OnInit {
+  @ViewChild("fallbackReceiptProcessingSettings")
+  public fallbackReceiptProcessingSettings!: AutocomleteComponent;
+
   public originalSystemSettings!: SystemSettings;
 
-  public allReceiptProcessingSettings!: ReceiptProcessingSettings[];
+  public allReceiptProcessingSettings: ReceiptProcessingSettings[] = [];
+
+  public filteredReceiptProcessingSettings: ReceiptProcessingSettings[] = [];
 
   constructor(private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute) {
     super();
@@ -32,6 +42,23 @@ export class SystemSettingsFormComponent extends BaseFormComponent implements On
       receiptProcessingSettingsId: [this.originalSystemSettings?.receiptProcessingSettingsId],
       fallbackReceiptProcessingSettingsId: [this.originalSystemSettings?.fallbackReceiptProcessingSettingsId]
     });
+
+    this.listenForReceiptProcessingSettingsChanges();
+  }
+
+  private listenForReceiptProcessingSettingsChanges(): void {
+    this.form.get("receiptProcessingSettingsId")?.valueChanges
+      .pipe(
+        untilDestroyed(this),
+        tap((value: number) => {
+          this.filteredReceiptProcessingSettings = this.allReceiptProcessingSettings.filter((rps) => rps.id !== value);
+
+          if (!value) {
+            this.fallbackReceiptProcessingSettings.clearFilter();
+          }
+        })
+      )
+      .subscribe();
   }
 
   public displayWith(id: number): string {
