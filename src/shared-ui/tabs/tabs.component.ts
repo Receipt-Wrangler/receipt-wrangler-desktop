3cfe448ade42cd022644f4d6b7f2e7f5eb8e4e4b
Noah <navi0001@protonmail.com>
2024-05-19 11:49:58 -0400

    update tab logic

--- src/shared-ui/tabs/tabs.component.ts
+++ src/shared-ui/tabs/tabs.component.ts
@@ -1,38 +1,38 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
-import { TabConfig } from './tab-config.interface';
-import { NavigationEnd, Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { tap } from "rxjs";
+import { TabConfig } from "./tab-config.interface";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss'],
+  selector: "app-tabs",
+  templateUrl: "./tabs.component.html",
+  styleUrls: ["./tabs.component.scss"],
 })
 export class TabsComponent implements OnInit, OnChanges {
   @Input() public tabs: TabConfig[] = [];
 
-  public activeLink: string = '';
+  public activeLink: string = "";
 
   constructor(private router: Router) {}
 
   public ngOnChanges(changes: SimpleChanges): void {
-    if (changes['tabs']) {
+    if (changes["tabs"]) {
       this.setActiveTab();
     }
   }
+
   public ngOnInit(): void {
     this.listenToRouteChanges();
   }
 
   private setActiveTab(): void {
-    const url = this.router.url;
-    const tab = this.tabs.find((t) => url.includes(t.routerLink.split('/')[0]));
+    const urlParts = this.router.url.split("/");
+    urlParts.shift();
+    urlParts.shift();
+
+    const url = urlParts.join("/");
+    const tab = this.tabs.find((t) => url.includes(t.routerLink));
+
     this.activeLink = tab ? tab.routerLink : this.tabs[0].routerLink;
   }
 
