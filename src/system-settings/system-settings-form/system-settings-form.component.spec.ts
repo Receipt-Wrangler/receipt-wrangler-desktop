700a70f783b831fc2499bea5917a36c24047a1ac
Noah <navi0001@protonmail.com>
2024-05-19 15:55:45 -0400

    tests

--- src/system-settings/system-settings-form/system-settings-form.component.spec.ts
+++ src/system-settings/system-settings-form/system-settings-form.component.spec.ts
@@ -1,23 +1,114 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgxsModule } from "@ngxs/store";
+import { of } from "rxjs";
+import { SystemSettingsService } from "../../open-api";
+import { PipesModule } from "../../pipes";
+import { SnackbarService } from "../../services";
+import { SharedUiModule } from "../../shared-ui/shared-ui.module";
 
-import { SystemSettingsFormComponent } from './system-settings-form.component';
+import { SystemSettingsFormComponent } from "./system-settings-form.component";
 
-describe('SystemSettingsFormComponent', () => {
+describe("SystemSettingsFormComponent", () => {
   let component: SystemSettingsFormComponent;
   let fixture: ComponentFixture<SystemSettingsFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SystemSettingsFormComponent]
+      declarations: [SystemSettingsFormComponent],
+      imports: [
+        SharedUiModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        PipesModule,
+        NgxsModule.forRoot([])
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                allReceiptProcessingSettings: [],
+                systemSettings: {},
+                formConfig: {}
+              }
+            }
+          }
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(SystemSettingsFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("init form with no data", () => {
+    component.ngOnInit();
+
+    expect(component.form.value).toEqual({
+      enableLocalSignUp: null,
+      emailPollingInterval: null,
+      receiptProcessingSettingsId: null,
+      fallbackReceiptProcessingSettingsId: null
+    });
+  });
+
+  it("init form with data", () => {
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    activatedRoute.snapshot.data["systemSettings"] = {
+      enableLocalSignUp: true,
+      emailPollingInterval: 5,
+      receiptProcessingSettingsId: 1,
+      fallbackReceiptProcessingSettingsId: 2
+    };
+
+    component.ngOnInit();
+
+    expect(component.form.value).toEqual({
+      enableLocalSignUp: true,
+      emailPollingInterval: 5,
+      receiptProcessingSettingsId: 1,
+      fallbackReceiptProcessingSettingsId: 2
+    });
+  });
+
+  it("should submit form", () => {
+    const systemSettingsService = TestBed.inject(SystemSettingsService);
+    const snackbarService = TestBed.inject(SnackbarService);
+    const router = TestBed.inject(Router);
+
+    const updateSystemSettingsSpy = spyOn(systemSettingsService, "updateSystemSettings").and.returnValue(of(null as any));
+    const snackbarServiceSpy = spyOn(snackbarService, "success");
+    const routerSpy = spyOn(router, "navigate");
+
+    component.form.setValue({
+      enableLocalSignUp: true,
+      emailPollingInterval: "5",
+      receiptProcessingSettingsId: 1,
+      fallbackReceiptProcessingSettingsId: 2
+    });
+
+    component.submit();
+
+    expect(updateSystemSettingsSpy).toHaveBeenCalledWith({
+      enableLocalSignUp: true,
+      emailPollingInterval: 5,
+      receiptProcessingSettingsId: 1,
+      fallbackReceiptProcessingSettingsId: 2
+    });
+
+    expect(snackbarServiceSpy).toHaveBeenCalled();
+    expect(routerSpy).toHaveBeenCalled();
   });
 });
