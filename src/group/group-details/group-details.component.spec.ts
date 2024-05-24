8b5205d5dac349d10a2be1aed5fa215422b4ed24
Noah <navi0001@protonmail.com>
2024-05-24 08:15:26 -0400

    fix tests

--- src/group/group-details/group-details.component.spec.ts
+++ src/group/group-details/group-details.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { NgxsModule } from "@ngxs/store";
 
-import { GroupDetailsComponent } from './group-details.component';
+import { GroupDetailsComponent } from "./group-details.component";
 
-describe('GroupDetailsComponent', () => {
+describe("GroupDetailsComponent", () => {
   let component: GroupDetailsComponent;
   let fixture: ComponentFixture<GroupDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GroupDetailsComponent]
+      declarations: [GroupDetailsComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, CommonModule, NgxsModule.forRoot([])],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                group: { id: 1 },
+              },
+            },
+          },
+        },
+
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(GroupDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
