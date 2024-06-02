e4632f89708651a0ff5461a2ce0763153a9ceb4a
Noah <navi0001@protonmail.com>
2024-06-02 08:03:39 -0400

    fix tests

--- src/import/import-form/import-form.component.spec.ts
+++ src/import/import-form/import-form.component.spec.ts
@@ -1,23 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { ImportType } from "../../open-api";
+import { PipesModule } from "../../pipes";
 
-import { ImportFormComponent } from './import-form.component';
+import { ImportFormComponent } from "./import-form.component";
 
-describe('ImportFormComponent', () => {
+describe("ImportFormComponent", () => {
   let component: ImportFormComponent;
   let fixture: ComponentFixture<ImportFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ImportFormComponent]
+      declarations: [ImportFormComponent],
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        PipesModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ImportFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should init form", () => {
+    component.ngOnInit();
+
+    expect(component.form.value).toEqual({
+      importType: ImportType.ImportConfig
+    });
   });
 });
