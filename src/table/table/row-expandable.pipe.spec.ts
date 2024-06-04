a9c6882cce717d0886e1830dd3d5637bd68dcf2f
Noah <navi0001@protonmail.com>
2024-06-04 08:18:59 -0400

    row expandable pipe

---
+++ src/table/table/row-expandable.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RowExpandablePipe } from './row-expandable.pipe';
+
+describe('RowExpandablePipe', () => {
+  it('create an instance', () => {
+    const pipe = new RowExpandablePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
