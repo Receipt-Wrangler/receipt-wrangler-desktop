f7ce4b1dd89ed5c6104b2df497340e7436382f03
Noah <navi0001@protonmail.com>
2024-06-06 09:53:09 -0400

    update a few tests

--- src/shared-ui/pretty-json/pretty-json.component.spec.ts
+++ src/shared-ui/pretty-json/pretty-json.component.spec.ts
@@ -1,23 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { PrettyJsonPipe } from "../task-table/pretty-json.pipe";
 
-import { PrettyJsonComponent } from './pretty-json.component';
+import { PrettyJsonComponent } from "./pretty-json.component";
 
-describe('PrettyJsonComponent', () => {
+describe("PrettyJsonComponent", () => {
   let component: PrettyJsonComponent;
   let fixture: ComponentFixture<PrettyJsonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PrettyJsonComponent]
+      declarations: [PrettyJsonComponent, PrettyJsonPipe],
+      providers: [PrettyJsonPipe],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PrettyJsonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/shared-ui/task-table/pretty-json.pipe.spec.ts
+++ src/shared-ui/task-table/pretty-json.pipe.spec.ts
@@ -1,7 +1,7 @@
-import {PrettyJsonPipe} from './pretty-json.pipe';
-import {TestBed} from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
+import { PrettyJsonPipe } from "./pretty-json.pipe";
 
-describe('PrettyJsonPipe', () => {
+describe("PrettyJsonPipe", () => {
   let pipe: PrettyJsonPipe;
 
   beforeEach(() => {
@@ -14,7 +14,21 @@ describe('PrettyJsonPipe', () => {
     pipe = TestBed.inject(PrettyJsonPipe);
   });
 
-  it('create an instance', () => {
+  it("create an instance", () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it("should return empty string when value is not json", () => {
+    const result = pipe.transform("not json");
+    expect(result).toEqual("");
+  });
+
+  it("should return sanitized html when value is json", () => {
+    const json = {
+      key: "value"
+    };
+    const jsonString = JSON.stringify(json);
+    const result = pipe.transform(jsonString);
+    expect(result === "").toBeFalse();
   });
 });
