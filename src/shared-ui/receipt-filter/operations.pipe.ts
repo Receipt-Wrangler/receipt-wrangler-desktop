5d32f49f6f9f9ae8e6f60e22e7c9a7bbdd2f4664
Noah <navi0001@protonmail.com>
2024-07-05 17:21:10 -0400

    no more screaming lmao

--- src/shared-ui/receipt-filter/operations.pipe.ts
+++ src/shared-ui/receipt-filter/operations.pipe.ts
@@ -1,30 +1,40 @@
+import { Pipe, PipeTransform } from "@angular/core";
 import {
-  dateOperationOptions, listOperationOptions, numberOperationOptions, textOperationOptions,
+  dateOperationOptions,
+  listOperationOptions,
+  numberOperationOptions,
+  textOperationOptions,
   usersOperationOptions
 } from "src/constants";
-
-import { Pipe, PipeTransform } from "@angular/core";
+import { FilterOperation } from "src/open-api";
 
 @Pipe({
-  name: 'operations',
+  name: "operations",
 })
 export class OperationsPipe implements PipeTransform {
-  public transform(type: string): unknown[] {
+  private displayValues: { [key: string]: string } = {
+    [FilterOperation.Contains]: "Contains",
+    [FilterOperation.Equals]: "Equals",
+    [FilterOperation.GreaterThan]: "Greater than",
+    [FilterOperation.LessThan]: "Less than",
+  };
+
+  public transform(type: string): string[] {
     switch (type) {
-      case 'date':
-        return dateOperationOptions;
+      case "date":
+        return dateOperationOptions.map((option) => this.displayValues[option]);
 
-      case 'text':
-        return textOperationOptions;
+      case "text":
+        return textOperationOptions.map((option) => this.displayValues[option]);
 
-      case 'number':
-        return numberOperationOptions;
+      case "number":
+        return numberOperationOptions.map((option) => this.displayValues[option]);
 
-      case 'list':
-        return listOperationOptions;
+      case "list":
+        return listOperationOptions.map((option) => this.displayValues[option]);
 
-      case 'users':
-        return usersOperationOptions;
+      case "users":
+        return usersOperationOptions.map((option) => this.displayValues[option]);
 
       default:
         return [];
