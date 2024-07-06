9c79c337f410ef8eb659dfca5fb15899ec527ca9
Noah <navi0001@protonmail.com>
2024-07-06 11:13:22 -0400

    read file from image

--- src/shared-ui/image-viewer/image-viewer.component.ts
+++ src/shared-ui/image-viewer/image-viewer.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, HostListener, Input, Output } from "@angular/core";
+import { Component, EventEmitter, HostListener, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
 
 @Component({
   selector: "app-image-viewer",
   templateUrl: "./image-viewer.component.html",
   styleUrl: "./image-viewer.component.scss"
 })
-export class ImageViewerComponent {
+export class ImageViewerComponent implements OnChanges {
   @HostListener("wheel", ["$event"])
   public onWheel(event: WheelEvent) {
     this.wheel.emit(event);
@@ -13,7 +13,28 @@ export class ImageViewerComponent {
 
   @Input() public imageBase64?: string = "";
 
+  @Input() public imageFile?: File;
+
   @Input() public scale: number = 1;
 
   @Output() public wheel: EventEmitter<WheelEvent> = new EventEmitter<WheelEvent>();
+
+  public imageFileUrl: string = "";
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes["imageFile"] && changes["imageFile"].currentValue) {
+      this.setImageFileUrl(changes["imageFile"].currentValue);
+    }
+  }
+
+  private setImageFileUrl(file: File): void {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      this.imageFileUrl = (event?.target?.result ?? "") as string;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
+
+
