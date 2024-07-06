c2e6170af0b8972c29cee16a7e3df1349a8c5da5
Noah <navi0001@protonmail.com>
2024-07-06 09:43:22 -0400

    wip

--- src/carousel/carousel.module.ts
+++ src/carousel/carousel.module.ts
@@ -1,3 +1,4 @@
+import { DragDropModule } from "@angular/cdk/drag-drop";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
@@ -13,6 +14,7 @@ import { CarouselComponent } from "./carousel/carousel.component";
   imports: [
     ButtonModule,
     CommonModule,
+    DragDropModule,
     ImageCropperModule,
     MatButtonModule,
     MatIconModule,
