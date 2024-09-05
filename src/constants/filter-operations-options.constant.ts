225bf6a5cd9b25d1b5baf7f2d91c5e20110196a3
Noah <navi0001@protonmail.com>
2024-09-05 18:17:09 -0400

    allow operations to be removed

--- src/constants/filter-operations-options.constant.ts
+++ src/constants/filter-operations-options.constant.ts
@@ -1,21 +1,21 @@
 import { FilterOperation } from "../open-api";
 
 export const listOperationOptions = Object.values(FilterOperation).filter(
-  (k) => k === "CONTAINS" && !!k
+  (k) => k === "CONTAINS"
 );
 
 export const dateOperationOptions = Object.values(FilterOperation).filter(
-  (k) => !k.includes("CONTAINS") && !!k
+  (k) => !k.includes("CONTAINS")
 );
 
 export const numberOperationOptions = Object.values(FilterOperation).filter(
-  (k) => !k.includes("CONTAINS") && !!k
+  (k) => !k.includes("CONTAINS")
 );
 
 export const textOperationOptions = Object.values(FilterOperation).filter(
-  (k) => !k.includes("THAN") && !!k
+  (k) => !k.includes("THAN")
 );
 
 export const usersOperationOptions = Object.values(FilterOperation).filter(
-  (k) => k === "CONTAINS" && !!k
+  (k) => k === "CONTAINS"
 );
