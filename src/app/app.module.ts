3345c7d3f8aee391eea8300d945b0f33baa499a8
Noah <navi0001@protonmail.com>
2024-10-17 08:36:49 -0400

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AuthFormUtil } from "../auth";
 import { IconModule } from "../icon/icon.module";
 import { HttpInterceptorService } from "../interceptors/http-interceptor.service";
 import { LayoutModule } from "../layout/layout.module";
-import { LocaleModule } from "../locale/locale.module";
 import { ApiModule, Configuration } from "../open-api";
 import { PipesModule } from "../pipes";
 import { StoreModule } from "../store/store.module";
@@ -32,7 +31,6 @@ import { AppComponent } from "./app.component";
     HttpClientModule,
     IconModule,
     LayoutModule,
-    LocaleModule,
     MatSnackBarModule,
     MatTooltipModule,
     NgxMaskDirective,
