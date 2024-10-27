6247e255337d29be9f2f6e293f5c66bafa9e6c9a
Noah <navi0001@protonmail.com>
2024-10-27 10:32:20 -0400

    revert env changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,3 @@
 export const environment = {
   isProd: false,
-  apiUrl: process.env["API_URL"] || "http://localhost:3000",
-  version: process.env["VERSION"] || "unknown",
-  build_date: process.env["BUILD_DATE"] || "unknown",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
   isProd: true,
-  version: process.env["VERSION"] || "unknown",
-  build_date: process.env["BUILD_DATE"] || "unknown",
 };
