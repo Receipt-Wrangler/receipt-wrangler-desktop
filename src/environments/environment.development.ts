d1f81f3fa68c128a656aa2bd2382ab336e965563
Noah <navi0001@protonmail.com>
2024-10-27 09:42:41 -0400

    add to env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,6 @@
 export const environment = {
   isProd: false,
+  apiUrl: process.env["API_URL"] || "http://localhost:3000",
+  version: process.env["VERSION"] || "unknown",
+  build_date: process.env["BUILD_DATE"] || "unknown",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
   isProd: true,
+  version: process.env["VERSION"] || "unknown",
+  build_date: process.env["BUILD_DATE"] || "unknown",
 };
