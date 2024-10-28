4d757e9e0157f409d1f32ee91c03379abc803be4
Noah <navi0001@protonmail.com>
2024-10-28 08:31:35 -0400

    add version and build date

--- src/about/about/about.component.ts
+++ src/about/about/about.component.ts
@@ -1,15 +1,24 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { Select } from "@ngxs/store";
+import { Observable } from "rxjs";
+import { About } from "../../open-api/index";
 import { SharedUiModule } from "../../shared-ui/shared-ui.module";
+import { AboutState } from "../../store/about.state";
+import { StoreModule } from "../../store/store.module";
 
 @Component({
   selector: "app-about",
   standalone: true,
   imports: [
-    SharedUiModule
+    CommonModule,
+    SharedUiModule,
+    StoreModule
   ],
   templateUrl: "./about.component.html",
   styleUrl: "./about.component.scss"
 })
 export class AboutComponent {
+  @Select(AboutState.about) public about!: Observable<About>;
 
 }
