4481f3f3bf032a64709a18a5e353680bd4b4e3b0
Noah <navi0001@protonmail.com>
2024-10-28 08:55:46 -0400

    test

--- src/about/about/about.component.spec.ts
+++ src/about/about/about.component.spec.ts
@@ -1,23 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NgxsModule } from "@ngxs/store";
+import { AboutState } from "../../store/about.state";
 
-import { AboutComponent } from './about.component';
+import { AboutComponent } from "./about.component";
 
-describe('AboutComponent', () => {
+describe("AboutComponent", () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutComponent]
+      imports: [AboutComponent, HttpClientTestingModule, NgxsModule.forRoot([AboutState])],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
