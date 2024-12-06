61672e2c87eeb7c1028eb1e5bb19ab02744b4a71
Noah2315 <noah231515@gmail.com>
2024-12-06 14:08:24 +0000

    fix test

--- src/layout/header/header.component.spec.ts
+++ src/layout/header/header.component.spec.ts
@@ -2,12 +2,9 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { Router } from "@angular/router";
 import { NgxsModule, Store } from "@ngxs/store";
-import { of } from "rxjs";
 import { ToggleIsSidebarOpen } from "src/store/layout.state.actions";
-import { ApiModule, AuthService } from "../../open-api";
-import { Logout } from "../../store";
+import { ApiModule } from "../../open-api";
 import { HeaderComponent } from "./header.component";
 
 describe("HeaderComponent", () => {
@@ -40,21 +37,5 @@ describe("HeaderComponent", () => {
     component.toggleSidebar();
 
     expect(store).toHaveBeenCalledOnceWith(new ToggleIsSidebarOpen());
-  });
-
-  it("should log the user out", () => {
-    const router = spyOn(TestBed.inject(Router), "navigate");
-
-    const store = spyOn(TestBed.inject(Store), "dispatch");
-    store.and.returnValue(of(undefined) as any);
-
-    const authService = spyOn(TestBed.inject(AuthService), "logout");
-    authService.and.returnValue(of(undefined) as any);
-
-    component.logout();
-
-    expect(authService).toHaveBeenCalledTimes(1);
-    expect(store).toHaveBeenCalledOnceWith(new Logout());
-    expect(router).toHaveBeenCalledWith(["/"]);
   });
 });
