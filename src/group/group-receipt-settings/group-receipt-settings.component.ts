f41fd23e3beecdcf412e0004a2656feac06d8ea8
Noah2315 <noah231515@gmail.com>
2024-12-26 20:48:01 +0000

    add form

--- src/group/group-receipt-settings/group-receipt-settings.component.ts
+++ src/group/group-receipt-settings/group-receipt-settings.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Store } from "@ngxs/store";
+import { FormBuilder } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { take, tap } from "rxjs";
 import { BaseFormComponent } from "../../form/index";
-import { Group, GroupRole } from "../../open-api/index";
-import { SharedUiModule } from "../../shared-ui/shared-ui.module";
+import { Group, GroupRole, GroupsService } from "../../open-api/index";
+import { SnackbarService } from "../../services/index";
 import { GroupUtil } from "../../utils/index";
 
 @Component({
   selector: "app-group-receipt-settings",
-  standalone: true,
-  imports: [
-    SharedUiModule
-  ],
   templateUrl: "./group-receipt-settings.component.html",
   styleUrl: "./group-receipt-settings.component.scss"
 })
@@ -24,8 +21,11 @@ export class GroupReceiptSettingsComponent extends BaseFormComponent implements 
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private store: Store,
-    private groupUtil: GroupUtil
+    private formBuilder: FormBuilder,
+    private groupUtil: GroupUtil,
+    private groupsService: GroupsService,
+    private router: Router,
+    private snackbarService: SnackbarService
   ) {
     super();
   }
@@ -33,7 +33,20 @@ export class GroupReceiptSettingsComponent extends BaseFormComponent implements 
   public ngOnInit(): void {
     this.setFormConfigFromRoute(this.activatedRoute);
     this.setOriginalGroup();
+    this.initForm();
     this.canEdit = this.groupUtil.hasGroupAccess(this.originalGroup.id, GroupRole.Owner, false, false);
+  }
+
+  private initForm(): void {
+    const receiptSettings = this.originalGroup.groupReceiptSettings;
+    this.form = this.formBuilder.group({
+      hideImages: [receiptSettings.hideImages ?? false],
+      hideReceiptCategories: [receiptSettings.hideReceiptCategories ?? false],
+      hideReceiptTags: [receiptSettings.hideReceiptTags ?? false],
+      hideItemCategories: [receiptSettings.hideItemCategories ?? false],
+      hideItemTags: [receiptSettings.hideItemTags ?? false],
+      hideComments: [receiptSettings.hideComments ?? false],
+    });
   }
 
   private setOriginalGroup(): void {
@@ -42,5 +55,25 @@ export class GroupReceiptSettingsComponent extends BaseFormComponent implements 
   }
 
 
-  public submit(): void {}
+  public submit(): void {
+    if (this.form.valid) {
+      this.groupsService.updateGroupReceiptSettings(this.originalGroup.id,
+        this.form.value)
+        .pipe(
+          take(1),
+          tap(() => {
+            this.snackbarService.success("Receipt settings updated successfully");
+            this.router.navigate(
+              [`/groups/${this.originalGroup.id}/receipt-settings/view`],
+              {
+                queryParams: {
+                  tab: "receipt-settings"
+                }
+              }
+            );
+          })
+        )
+        .subscribe();
+    }
+  }
 }
