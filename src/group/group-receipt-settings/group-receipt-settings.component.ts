e750e7764ccd2e6a766aa9d0dc9d6a10480d43f5
Noah2315 <noah231515@gmail.com>
2024-12-26 18:23:45 +0000

    stub in the form

--- src/group/group-receipt-settings/group-receipt-settings.component.ts
+++ src/group/group-receipt-settings/group-receipt-settings.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Store } from "@ngxs/store";
+import { BaseFormComponent } from "../../form/index";
+import { Group, GroupRole } from "../../open-api/index";
+import { SharedUiModule } from "../../shared-ui/shared-ui.module";
+import { GroupUtil } from "../../utils/index";
 
 @Component({
-  selector: 'app-group-receipt-settings',
+  selector: "app-group-receipt-settings",
   standalone: true,
-  imports: [],
-  templateUrl: './group-receipt-settings.component.html',
-  styleUrl: './group-receipt-settings.component.scss'
+  imports: [
+    SharedUiModule
+  ],
+  templateUrl: "./group-receipt-settings.component.html",
+  styleUrl: "./group-receipt-settings.component.scss"
 })
-export class GroupReceiptSettingsComponent {
+export class GroupReceiptSettingsComponent extends BaseFormComponent implements OnInit {
+  public originalGroup!: Group;
 
+  public editLink: string = "";
+
+  public canEdit = false;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private store: Store,
+    private groupUtil: GroupUtil
+  ) {
+    super();
+  }
+
+  public ngOnInit(): void {
+    this.setFormConfigFromRoute(this.activatedRoute);
+    this.setOriginalGroup();
+    this.canEdit = this.groupUtil.hasGroupAccess(this.originalGroup.id, GroupRole.Owner, false, false);
+  }
+
+  private setOriginalGroup(): void {
+    this.originalGroup = this.activatedRoute.snapshot.data["group"];
+    this.editLink = `/groups/${this.originalGroup.id}/receipt-settings/edit`;
+  }
+
+
+  public submit(): void {}
 }
