8d9195385bbe3c55c136a33d97c6a1c520a4331b
Noah231515 <noah231515@gmail.com>
2025-01-21 19:36:38 +0000

    fix tests

--- src/dashboard/activity/activity.component.spec.ts
+++ src/dashboard/activity/activity.component.spec.ts
@@ -1,23 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ScrollingModule } from "@angular/cdk/scrolling";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatListModule } from "@angular/material/list";
+import { SharedUiModule } from "../../shared-ui/shared-ui.module";
+import { DashboardListComponent } from "../dashboard-list/dashboard-list.component";
 
-import { ActivityComponent } from './activity.component';
+import { ActivityComponent } from "./activity.component";
 
-describe('ActivityComponent', () => {
+describe("ActivityComponent", () => {
   let component: ActivityComponent;
   let fixture: ComponentFixture<ActivityComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ActivityComponent]
+      declarations: [ActivityComponent, DashboardListComponent],
+      imports: [SharedUiModule, HttpClientTestingModule, ScrollingModule, MatListModule]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ActivityComponent);
     component = fixture.componentInstance;
+    component.widget = { name: "" } as any;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
