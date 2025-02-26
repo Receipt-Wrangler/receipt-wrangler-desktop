7d547d084343ce8f91fcf6dcabd12274cef15de6
noah231515 <noah231515@gmail.com>
2025-02-26 14:05:11 +0000

    fix tests

--- src/services/receipt-export.service.spec.ts
+++ src/services/receipt-export.service.spec.ts
@@ -1,16 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
 
-import { ReceiptExportService } from './receipt-export.service';
+import { ReceiptExportService } from "./receipt-export.service";
 
-describe('ReceiptExportService', () => {
+describe("ReceiptExportService", () => {
   let service: ReceiptExportService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(ReceiptExportService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
--- src/standalone/components/export-button/export-button.component.spec.ts
+++ src/standalone/components/export-button/export-button.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from "@angular/router";
 
-import { ExportButtonComponent } from './export-button.component';
+import { ExportButtonComponent } from "./export-button.component";
 
-describe('ExportButtonComponent', () => {
+describe("ExportButtonComponent", () => {
   let component: ExportButtonComponent;
   let fixture: ComponentFixture<ExportButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExportButtonComponent]
+      imports: [ExportButtonComponent],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {}
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ExportButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
