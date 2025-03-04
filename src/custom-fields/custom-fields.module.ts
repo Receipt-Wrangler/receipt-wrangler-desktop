e75622de1f39e51b0d3189a3af0202f21d76db18
noah231515 <noah231515@gmail.com>
2025-03-04 12:50:11 +0000

    export selected receipts

---
+++ src/custom-fields/custom-fields.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { CustomFieldsRoutingModule } from "./custom-fields-routing.module";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomFieldsRoutingModule
+  ]
+})
+export class CustomFieldsModule {}
