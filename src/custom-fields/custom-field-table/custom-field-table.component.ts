dc781bb7a110d4727fb8dad54d374174e54ad228
noah231515 <noah231515@gmail.com>
2025-03-07 13:24:42 +0000

    comment

--- src/custom-fields/custom-field-table/custom-field-table.component.ts
+++ src/custom-fields/custom-field-table/custom-field-table.component.ts
@@ -1,14 +1,16 @@
 import { Component, TemplateRef, ViewChild } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { PageEvent } from "@angular/material/paginator";
+import { Sort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
-import { Select } from "@ngxs/store";
-import { Observable, of, switchMap, take, tap } from "rxjs";
+import { Select, Store } from "@ngxs/store";
+import { Observable, take, tap } from "rxjs";
 import { PagedTableInterface } from "src/interfaces/paged-table.interface";
 import { PagedDataDataInner, PagedRequestCommand } from "src/open-api";
 import { CategoryTableState } from "src/store/category-table.state";
 import { TableComponent } from "src/table/table/table.component";
-import { DEFAULT_DIALOG_CONFIG } from "../../constants/index";
-import { ConfirmationDialogComponent } from "../../shared-ui/confirmation-dialog/confirmation-dialog.component";
+import { SnackbarService } from "../../services/index";
+import { SetOrderBy, SetPage, SetPageSize, SetSortDirection } from "../../store/custom-field-table.state.actions";
 import { TableColumn } from "../../table/table-column.interface";
 
 @Component({
@@ -43,6 +45,12 @@ export class CustomFieldTableComponent {
   public totalCount: number = 0;
 
   public headerText: string = "Categories";
+
+  constructor(
+    private store: Store,
+    private matDialog: MatDialog,
+    private snackbarService: SnackbarService,
+  ) {}
 
   public ngOnInit(): void {
     this.initTableData();
@@ -140,69 +148,69 @@ export class CustomFieldTableComponent {
     ];
   }
 
-  public openEditDialog(categoryView: CategoryView): void {
-    const dialogRef = this.matDialog.open(CategoryForm, DEFAULT_DIALOG_CONFIG);
+  /*  public openEditDialog(categoryView: CategoryView): void {
+      const dialogRef = this.matDialog.open(CategoryForm, DEFAULT_DIALOG_CONFIG);
 
-    dialogRef.componentInstance.category = categoryView;
-    dialogRef.componentInstance.headerText = `Edit ${categoryView.name}`;
+      dialogRef.componentInstance.category = categoryView;
+      dialogRef.componentInstance.headerText = `Edit ${categoryView.name}`;
 
-    dialogRef
-      .afterClosed()
-      .pipe(
-        take(1),
-        tap((refreshData) => {
-          if (refreshData) {
-            this.getCategories();
-          }
-        })
-      )
-      .subscribe();
-  }
+      dialogRef
+        .afterClosed()
+        .pipe(
+          take(1),
+          tap((refreshData) => {
+            if (refreshData) {
+              this.getCategories();
+            }
+          })
+        )
+        .subscribe();
+    }
 
-  public openAddDialog(): void {
-    const dialogRef = this.matDialog.open(CategoryForm, DEFAULT_DIALOG_CONFIG);
+    public openAddDialog(): void {
+      const dialogRef = this.matDialog.open(CategoryForm, DEFAULT_DIALOG_CONFIG);
 
-    dialogRef.componentInstance.headerText = `Add category`;
+      dialogRef.componentInstance.headerText = `Add category`;
 
-    dialogRef
-      .afterClosed()
-      .pipe(
-        take(1),
-        tap((refreshData) => {
-          if (refreshData) {
-            this.getCategories();
-          }
-        })
-      )
-      .subscribe();
-  }
+      dialogRef
+        .afterClosed()
+        .pipe(
+          take(1),
+          tap((refreshData) => {
+            if (refreshData) {
+              this.getCategories();
+            }
+          })
+        )
+        .subscribe();
+    }*/
 
-  public openDeleteConfirmationDialog(categoryView: CategoryView) {
-    const dialogRef = this.matDialog.open(
-      ConfirmationDialogComponent,
-      DEFAULT_DIALOG_CONFIG
-    );
+  /*  public openDeleteConfirmationDialog(categoryView: CategoryView) {
+      const dialogRef = this.matDialog.open(
+        ConfirmationDialogComponent,
+        DEFAULT_DIALOG_CONFIG
+      );
 
-    dialogRef.componentInstance.headerText = `Delete ${categoryView.name}`;
-    dialogRef.componentInstance.dialogContent = `Are you sure you want to delete ${categoryView.name}? This action is irreversiable and will remove this category from the receipts it is associated with.`;
+      dialogRef.componentInstance.headerText = `Delete ${categoryView.name}`;
+      dialogRef.componentInstance.dialogContent = `Are you sure you want to delete ${categoryView.name}? This action is irreversiable and will remove this category from the receipts it is associated with.`;
 
-    dialogRef
-      .afterClosed()
-      .pipe(
-        take(1),
-        switchMap((confirmed) => {
-          if (confirmed) {
-            return this.categoryService.deleteCategory(categoryView.id).pipe(
-              tap(() => {
-                this.snackbarService.success("Category successfully deleted");
-                this.getCategories();
-              })
-            );
-          } else {
-            return of(undefined);
-          }
-        })
-      )
-      .subscribe();
-  }
+      dialogRef
+        .afterClosed()
+        .pipe(
+          take(1),
+          switchMap((confirmed) => {
+            if (confirmed) {
+              return this.categoryService.deleteCategory(categoryView.id).pipe(
+                tap(() => {
+                  this.snackbarService.success("Category successfully deleted");
+                  this.getCategories();
+                })
+              );
+            } else {
+              return of(undefined);
+            }
+          })
+        )
+        .subscribe();
+    }*/
 }
