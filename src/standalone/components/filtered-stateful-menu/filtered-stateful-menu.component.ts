eaac45878a3df299c81b3ab37439b7f75a5545fd
noah231515 <noah231515@gmail.com>
2025-03-18 13:00:26 +0000

    implement filtering

--- src/standalone/components/filtered-stateful-menu/filtered-stateful-menu.component.ts
+++ src/standalone/components/filtered-stateful-menu/filtered-stateful-menu.component.ts
@@ -1,13 +1,16 @@
 import { CdkMenu, CdkMenuTrigger } from "@angular/cdk/menu";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { MatCheckbox } from "@angular/material/checkbox";
 import { MatMenuItem } from "@angular/material/menu";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { tap } from "rxjs";
 import { BaseButtonComponent } from "../../../button/base-button/base-button.component";
 import { ButtonModule } from "../../../button/index";
 import { InputModule } from "../../../input/index";
 import { StatefulMenuItem } from "./stateful-menu-item";
 
+@UntilDestroy()
 @Component({
   selector: "app-filtered-stateful-menu",
   imports: [
@@ -18,12 +21,40 @@ import { StatefulMenuItem } from "./stateful-menu-item";
   templateUrl: "./filtered-stateful-menu.component.html",
   styleUrl: "./filtered-stateful-menu.component.scss",
 })
-export class FilteredStatefulMenuComponent extends BaseButtonComponent {
+export class FilteredStatefulMenuComponent extends BaseButtonComponent implements OnInit, OnChanges {
   @Input() public items: StatefulMenuItem[] = [];
+
+  @Input() public filterFunc = (item: StatefulMenuItem, filter: string) => item.displayValue.toLowerCase().includes(filter.toLowerCase());
 
   @Output() public itemSelected = new EventEmitter<StatefulMenuItem>();
 
   public filterFormControl = new FormControl();
+
+  public filteredItems: StatefulMenuItem[] = [];
+
+  public ngOnInit(): void {
+    this.listenToFilterFormChanges();
+  }
+
+  private listenToFilterFormChanges(): void {
+    this.filterFormControl.valueChanges.pipe(
+      untilDestroyed(this),
+      tap((filter) => {
+        if (filter) {
+          this.filteredItems = this.filteredItems.filter(item => this.filterFunc(item, filter));
+        } else {
+          this.filteredItems = Array.from(this.items);
+        }
+      })
+    )
+      .subscribe();
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes["items"].currentValue) {
+      this.filteredItems = changes["items"].currentValue;
+    }
+  }
 
   public onItemSelected(item: StatefulMenuItem, event: MouseEvent) {
     event.stopPropagation();
