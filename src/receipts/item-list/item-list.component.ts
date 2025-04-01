1268f5607f639044352ea636fae515cc0152f361
noah231515 <noah231515@gmail.com>
2025-04-01 12:30:41 +0000

    stub stuff in

--- src/receipts/item-list/item-list.component.ts
+++ src/receipts/item-list/item-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { FormArray, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-item-list',
+  selector: "app-item-list",
   standalone: false,
-  templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.scss'
+  templateUrl: "./item-list.component.html",
+  styleUrl: "./item-list.component.scss"
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnChanges {
+  @Input() public form!: FormGroup;
 
+  public itemsFormArray?: FormArray;
+
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes["form"].currentValue) {
+      this.itemsFormArray = this.form.get("receiptItems") as FormArray;
+    }
+  }
 }
