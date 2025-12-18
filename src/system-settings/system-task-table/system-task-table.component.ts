4e2e733e2fc3ab81fa50f787b7eee1c0bd70cbb1
noah231515 <noah231515@gmail.com>
2025-12-18 13:15:46 +0000

    fix bad injector

--- src/system-settings/system-task-table/system-task-table.component.ts
+++ src/system-settings/system-task-table/system-task-table.component.ts
@@ -1,18 +1,18 @@
 import { Component } from "@angular/core";
-import { BaseTableService } from "../../services/base-table.service";
+import { TABLE_SERVICE_INJECTION_TOKEN } from "../../services/injection-tokens/table-service";
 import { SystemTaskTableService } from "../../services/system-task-table.service";
 
 @Component({
-    selector: "app-system-task-table",
-    templateUrl: "./system-task-table.component.html",
-    styleUrl: "./system-task-table.component.scss",
-    providers: [
-        {
-            provide: BaseTableService,
-            useClass: SystemTaskTableService
-        }
-    ],
-    standalone: false
+  selector: "app-system-task-table",
+  templateUrl: "./system-task-table.component.html",
+  styleUrl: "./system-task-table.component.scss",
+  providers: [
+    {
+      provide: TABLE_SERVICE_INJECTION_TOKEN,
+      useClass: SystemTaskTableService
+    },
+  ],
+  standalone: false
 })
 export class SystemTaskTableComponent {
 }
